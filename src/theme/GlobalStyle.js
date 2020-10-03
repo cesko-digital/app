b589ebf7d5199f248fc707fe024eeaaa25a5bbed
Horm <horm@horm.codes>
2020-10-03 19:11:57 +0200

    Edit lint rules and fix related issues

--- src/theme/GlobalStyle.js
+++ src/theme/GlobalStyle.js
@@ -4,6 +4,6 @@ export const GlobalStyle = createGlobalStyle`
   html, body {
     margin: 0;
     padding: 0;
-    font-family: ${props => props.theme.fonts.body};
+    font-family: ${(props) => props.theme.fonts.body};
   }
 `
