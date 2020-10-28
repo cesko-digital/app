f32212129c61d208a15c593335fb96444c3700a2
Miroslav Foltýn <miroslav.foltyn@intelstudios.com>
2020-10-28 15:15:42 +0100

    feat(footer): Add unfinished footer without newsletter form

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -1,15 +1,76 @@
-import React, { useContext } from 'react'
-import Section from '../section'
-import { ThemeContext } from 'styled-components'
+import React from 'react'
+import { ButtonSize } from 'components/buttons'
+import { Link } from 'components/links'
 import * as S from './styles'
 
 const Footer: React.FC = () => {
-  const theme = useContext(ThemeContext)
+  const t = {
+    headings: {
+      ceskoDigital: 'Česko.Digital',
+      online: 'Online',
+      newsletter: 'Newsletter',
+    },
+    info: {
+      projects: ['Projekty', '#'],
+      blog: ['Blog', '#'],
+      loginToSlack: ['Přihlásit se do Slacku', '#'],
+      submitProject: ['Zadat projekt', '#'],
+      contribute: ['Přispět', '#'],
+    },
+    social: {
+      facebook: ['Facebook', '#'],
+      twitter: ['Twitter', '#'],
+      github: ['GitHub', '#'],
+      slack: ['Slack', '#'],
+    },
+    newsletter: {
+      note:
+        'Chcete vědět, na čem pracujeme? Jednou za měsíc shrneme, co se v komunitě událo a co chystáme.',
+      inputPlaceholder: 'Zadejte e-mail',
+      inputErr: 'Zadejte prosím validní e-mailovou adresu.',
+      subscribe: 'Odebírat',
+    },
+    footnote: 'cesko.digital © 2020, Tento web používa cookies ¯\\_(ツ)_/¯',
+  }
 
   return (
-    <Section as={'footer'} backgroundColor={theme.colors.darkGrey}>
-      <S.Content verticalPadding={40}>Footer</S.Content>
-    </Section>
+    <S.Wrapper>
+      <S.Outer>
+        <S.Container>
+          <S.Info>
+            <S.InfoBlock>
+              <S.Heading>{t.headings.ceskoDigital}</S.Heading>
+              <S.Navigation>
+                <S.Links>
+                  {Object.values(t.info).map(([name, url], i) => (
+                    <S.LinkItem key={i}>
+                      <Link size={ButtonSize.Small} to={url}>
+                        {name}
+                      </Link>
+                    </S.LinkItem>
+                  ))}
+                </S.Links>
+              </S.Navigation>
+            </S.InfoBlock>
+            <S.InfoBlock>
+              <S.Heading>{t.headings.online}</S.Heading>
+              <S.Navigation>
+                <S.Links>
+                  {Object.values(t.social).map(([name, url], i) => (
+                    <S.LinkItem key={i}>
+                      <Link size={ButtonSize.Small} to={url} key={i}>
+                        {name}
+                      </Link>
+                    </S.LinkItem>
+                  ))}
+                </S.Links>
+              </S.Navigation>
+            </S.InfoBlock>
+          </S.Info>
+          <S.Note>{t.footnote}</S.Note>
+        </S.Container>
+      </S.Outer>
+    </S.Wrapper>
   )
 }
 
