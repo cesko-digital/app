500fb6cc01a3698c6a6930e36c5df6c29d135773
patrikbraborec <patrik.braborec@gooddata.com>
2020-10-28 23:04:50 +0100

    Add new component new-project

---
+++ src/components/cards/new-project/index.stories.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+import NewProject, { NewProjectProps } from '.'
+
+const story: Meta = {
+  title: 'Cards/NewProject',
+  component: NewProject,
+  argTypes: {
+    name: {
+      defaultValue: 'Mám nápad na projekt',
+      control: {
+        type: 'text',
+      },
+    },
+    description: {
+      defaultValue:
+        'Rozvíjíme nápady, které skrze IT pomáhají zlepšovat život v Česku, nemají komerční alternativu a jsou udržitelné. Máte takový projekt, nebo na něm pracujete, ale chybí vám expertní dobrovolníci?',
+      control: {
+        type: 'text',
+      },
+    },
+    linkUrl: {
+      defaultValue: 'https://cesko-digital.slack.com/archives/CHG9NA23D',
+      control: {
+        type: 'text',
+      },
+    },
+    linkText: {
+      defaultValue: 'Zadat projekt',
+      control: {
+        type: 'text',
+      },
+    },
+  },
+}
+
+const Template: Story<NewProjectProps> = (args: NewProjectProps) => (
+  <NewProject {...args} />
+)
+
+export const Default = Template.bind({})
+
+export default story
