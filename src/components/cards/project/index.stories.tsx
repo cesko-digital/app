973572bfe61f7e9d7912bb2b68415cff2ee59dfb
Haaxor1689 <betkomaros@gmail.com>
2020-11-02 20:23:39 +0100

    Add project card component

---
+++ src/components/cards/project/index.stories.tsx
@@ -0,0 +1,25 @@
+import React, { ComponentProps } from 'react'
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react'
+
+import ProjectCard from '.'
+
+const story: Meta = {
+  title: 'Cards/ProjectCard',
+  component: ProjectCard,
+}
+
+export const Default: Story<ComponentProps<typeof ProjectCard>> = (args) => (
+  <ProjectCard {...args} />
+)
+Default.args = {
+  title: 'Project name',
+  description:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  cover: 'https://via.placeholder.com/400x200',
+  logo: 'https://via.placeholder.com/100',
+  link: '#',
+  tags: ['#javascript'],
+}
+
+export default story
---
+++ src/components/cards/project/index.tsx
@@ -0,0 +1,50 @@
+import React, { FC } from 'react'
+import * as S from './styles'
+import { Link } from 'components/links'
+import { useTranslation } from 'gatsby-plugin-react-i18next'
+
+interface ProjectCardProps {
+  title: string
+  description: string
+  cover: string
+  logo: string
+  link: string
+  tags: string[]
+}
+
+const ProjectCard: FC<ProjectCardProps> = ({
+  title,
+  description,
+  cover,
+  logo,
+  link,
+  tags,
+}) => {
+  const { t } = useTranslation()
+  return (
+    <S.Card>
+      <S.Header>
+        <S.Cover
+          url={cover}
+          aria-label={`${t('cards.project.coverAriaLabel')} ${title}`}
+        />
+        <S.Logo
+          url={logo}
+          aria-label={`${t('cards.project.logoAriaLabel')}  ${title}`}
+        />
+      </S.Header>
+      <S.Content>
+        <S.Title>{title}</S.Title>
+        <S.TagList>
+          {tags.map((tag) => (
+            <S.Tag key={tag}>{tag}</S.Tag>
+          ))}
+        </S.TagList>
+        <S.Description>{description}</S.Description>
+        <Link to={link}>{t('cards.project.projectDetail').toString()}</Link>
+      </S.Content>
+    </S.Card>
+  )
+}
+
+export default ProjectCard
