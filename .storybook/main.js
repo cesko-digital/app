def033ae5d3559a168c88b7faf18e619736dc29c
Andrei Fidelman <andrei.fidelman@ataccama.com>
2020-11-16 20:58:25 +0100

    Implement button and link components (also fix Storybook config)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,4 @@
 module.exports = {
-  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/preset-typescript',
-  ],
+  stories: ['../src/**/*.stories.tsx'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 }
---
+++ src/components/links/button-as-link/index.stories.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import { Story, Meta } from '@storybook/react'
+
+import ButtonAsLink, { ButtonAsLinkProps } from '.'
+import { ButtonSize } from '../../buttons'
+
+const story: Meta = {
+  title: 'Links/ButtonAsLink',
+  component: ButtonAsLink,
+  argTypes: {
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+    size: {
+      defaultValue: ButtonSize.Normal,
+      control: {
+        type: 'select',
+        options: [ButtonSize.Normal, ButtonSize.Small],
+      },
+    },
+    inverted: {
+      defaultValue: false,
+      control: {
+        type: 'boolean',
+      },
+    },
+    to: {
+      defaultValue: 'https://cesko.digital/',
+      control: {
+        type: 'text',
+      },
+    },
+  },
+}
+
+const Template: Story<ButtonAsLinkProps> = (args: ButtonAsLinkProps) => (
+  <ButtonAsLink {...args} />
+)
+
+export const Default = Template.bind({})
+Default.args = {
+  children: 'Button',
+}
+
+export const Inverted = Template.bind({})
+Inverted.args = {
+  children: 'Button',
+  inverted: true,
+}
+
+export const Small = Template.bind({})
+Small.args = {
+  children: 'Button',
+  size: ButtonSize.Small,
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  children: 'Button',
+  disabled: true,
+}
+
+export default story
