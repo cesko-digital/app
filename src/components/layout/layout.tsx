5dd1a3b40dcd5f802453d5611a001c72010468d0
Marek Lisy <marek.lisy.hk@gmail.com>
2020-12-22 18:56:45 +0100

    Breadcrumb component + stories + Layout integration + page-2 demo
    fix prettier format

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -3,17 +3,28 @@ import React, { ReactNode } from 'react'
 import Head from 'components/head'
 import Header from './header'
 import Footer from './footer'
+import Section from './section'
+import SectionContent from './section-content'
+import Breadcrumb, { Crumb } from './breadcrumb'
 import * as S from './styles'
 
 export interface LayoutProps {
+  crumbs?: Crumb[]
   children: ReactNode
 }
 
-const Layout: React.FC<LayoutProps> = ({ children }: LayoutProps) => {
+const Layout: React.FC<LayoutProps> = ({ crumbs, children }: LayoutProps) => {
   return (
     <S.Container>
       <Head />
       <Header />
+      {crumbs && (
+        <Section>
+          <SectionContent>
+            <Breadcrumb crumbs={crumbs} />
+          </SectionContent>
+        </Section>
+      )}
       <main>{children}</main>
       <Footer />
     </S.Container>
