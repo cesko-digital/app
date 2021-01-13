cdb05e03fb42314eea1567d4c40e7a97223b9f1b
Horm <horm@horm.codes>
2021-01-13 18:36:04 +0100

    Simplify layout conditional rendering for breadcrumbs

---
+++ src/components/layout/breadcrumb/__tests__/helpers.spec.ts
@@ -0,0 +1,46 @@
+import { areCrumbsValid, transformCrumbs } from '../helpers'
+import { Crumb } from '../index'
+
+describe('helpers', () => {
+  describe('transformCrumbs', () => {
+    it('should return object with last element and remaining items', () => {
+      const LAST_CRUMB = { label: 'Test 3', path: '/test-3' }
+      const REMAINING_CRUMBS: Crumb[] = [
+        { label: 'Test', path: '/test' },
+        { label: 'Test 2', path: '/test-2' },
+      ]
+      expect(transformCrumbs([...REMAINING_CRUMBS, LAST_CRUMB])).toEqual({
+        lastCrumb: LAST_CRUMB,
+        firstCrumbs: REMAINING_CRUMBS,
+      })
+    })
+
+    it('should not mutate passed array', () => {
+      const CRUMBS: Crumb[] = [
+        { label: 'Test', path: '/test' },
+        { label: 'Test 2', path: '/test-2' },
+      ]
+      transformCrumbs(CRUMBS)
+      expect(CRUMBS).toEqual([
+        { label: 'Test', path: '/test' },
+        { label: 'Test 2', path: '/test-2' },
+      ])
+    })
+  })
+
+  describe('areCrumbsValid', () => {
+    it('should return true for non empty array', () => {
+      expect(areCrumbsValid([{ label: 'Test', path: '/test' }])).toBeTruthy()
+      expect(
+        areCrumbsValid([
+          { label: 'Test', path: '/test' },
+          { label: 'Test 2', path: '/test-2' },
+        ])
+      ).toBeTruthy()
+    })
+
+    it('should return false for empty array', () => {
+      expect(areCrumbsValid([])).toBeFalsy()
+    })
+  })
+})
