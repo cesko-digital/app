301dad7d21b9d620ca2640aeb38c97b9d1ee6561
Horm <horm@horm.codes>
2021-01-18 18:52:40 +0100

    Fix font flickering on React state update

--- src/theme/index.tsx
+++ src/theme/index.tsx
@@ -4,6 +4,8 @@ import { DefaultTheme, ThemeProvider } from 'styled-components'
 import { defaultTheme } from './default'
 import { GlobalStyle } from './global-style'
 
+import './fonts.css' // Adding fonts in createGlobalStyle is introducing font flickering
+
 interface Props {
   theme?: DefaultTheme
   children: ReactNode
