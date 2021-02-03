d809dba90a915f2018ddfb56d410a6dbea41bb8a
Martina Hytychová <martina.hyt@gmail.com>
2021-02-03 19:35:47 +0100

    Completition and implementation - section Join Us

--- src/components/cards/index.ts
+++ src/components/cards/index.ts
@@ -1,3 +1,3 @@
 export { default as NewProject } from './new-project'
 export { default as Project } from './project'
-export { default as PaneVolunteer } from './pane-volunteer'
+export { default as PanelVolunteer } from './panel-volunteer'
--- src/components/cards/new-project/index.tsx
+++ src/components/cards/new-project/index.tsx
@@ -3,27 +3,31 @@ import * as S from './styles'
 import ButtonAsLink from '../../links/button-as-link'
 import { BulbIcon } from 'components/icons'
 
-export interface NewProjectProps {
+interface Project {
   name: string
   description: string
   linkUrl: string
   linkText: string
 }
 
-const NewProject: React.FC<NewProjectProps> = ({
-  name,
-  description,
-  linkUrl,
-  linkText,
-}: NewProjectProps) => (
-  <S.Wrapper>
-    <BulbIcon />
-    <S.Title>{name}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.ButtonWrapper>
-      <ButtonAsLink to={linkUrl}>{linkText}</ButtonAsLink>
-    </S.ButtonWrapper>
-  </S.Wrapper>
-)
+const NewProject: React.FC = () => {
+  const t: Project = {
+    name: 'Mám nápad na projekt',
+    description:
+      'Rozvíjíme nápady, které skrze IT pomáhají zlepšovat život v Česku, nemají komerční alternativu a jsou udržitelné. Máte takový projekt, nebo na něm pracujete, ale chybí vám expertní dobrovolníci?',
+    linkUrl: 'https://cesko-digital.slack.com/archives/CHG9NA23D',
+    linkText: 'Zadat projekt',
+  }
+  return (
+    <S.Wrapper>
+      <BulbIcon />
+      <S.Title>{t.name}</S.Title>
+      <S.Description>{t.description}</S.Description>
+      <S.ButtonWrapper>
+        <ButtonAsLink to={t.linkUrl}>{t.linkText}</ButtonAsLink>
+      </S.ButtonWrapper>
+    </S.Wrapper>
+  )
+}
 
 export default NewProject
