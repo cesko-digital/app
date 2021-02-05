a7e38995f01dbdb1fcdad690514fbe905bb8428c
Horm <horm@horm.codes>
2021-02-05 21:33:10 +0100

    Fix padding for 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 
-import { Layout } from 'components/layout'
+import { Layout, Section, SectionContent } from 'components/layout'
 import SEO from 'components/seo'
 
 const NotFoundPage: React.FC = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Section>
+      <SectionContent>
+        <SEO title="404: Not found" />
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </SectionContent>
+    </Section>
   </Layout>
 )
 
