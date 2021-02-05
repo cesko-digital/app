bb156ee5f7af946f131cc80b1fe8cbc25df006a4
Horm <horm@horm.codes>
2021-02-05 21:45:50 +0100

    Fix jest absolute imports

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,10 @@ module.exports = {
     __PATH_PREFIX__: ``,
   },
   moduleNameMapper: {
+    'theme/(.*)': '<rootDir>/src/theme/$1',
+    'icons/(.*)': '<rootDir>/src/icons/$1',
+    'components/(.*)': '<rootDir>/src/components/$1',
+    'images/(.*)': '<rootDir>/src/images/$1',
     '.+\\.(css|styl|less|sass|scss)$': `identity-obj-proxy`,
     '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': `<rootDir>/__mocks__/file-mock.js`,
   },
