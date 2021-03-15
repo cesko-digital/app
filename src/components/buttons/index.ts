9f978b606e53787525edc2d03327778398d5e606
Martina Hytychová <martina.hyt@gmail.com>
2021-03-15 19:19:01 +0100

    Implementation Contribute section Project Detail page

--- src/components/buttons/index.ts
+++ src/components/buttons/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './button'
 export { default as LinkAsButton } from './link-as-button'
+export { default as SlackButton } from './slack-button'
 export { ButtonSize } from './button/enums'
