02b8b96b300a8d30c1b98547c29a097345177620
Horm <horm@horm.codes>
2021-03-17 17:31:30 +0100

    formatting

--- src/page-components/homepage/sections/image-gallery/index.tsx
+++ src/page-components/homepage/sections/image-gallery/index.tsx
@@ -11,46 +11,47 @@ const imageTreeTest = [
         type: 'row',
         subset: [
           {
-            type: "img",
-            url: "https://via.placeholder.com/2000x1200",
-            alt: "...",
-            ratio: 66
-          }, {
-            type: "img",
-            url: "https://via.placeholder.com/500",
-            alt: "...",
-            ratio: 33
-          }
-        ]
+            type: 'img',
+            url: 'https://via.placeholder.com/2000x1200',
+            alt: '...',
+            ratio: 66,
+          },
+          {
+            type: 'img',
+            url: 'https://via.placeholder.com/500',
+            alt: '...',
+            ratio: 33,
+          },
+        ],
       },
       {
         type: 'row',
         subset: [
           {
-            type: "img",
-            url: "https://via.placeholder.com/15",
-            alt: "...",
-            ratio: 33
+            type: 'img',
+            url: 'https://via.placeholder.com/15',
+            alt: '...',
+            ratio: 33,
           },
           {
-            type: "img",
-            url: "https://via.placeholder.com/1110",
-            alt: "...",
-            ratio: 66
-          }
-        ]
+            type: 'img',
+            url: 'https://via.placeholder.com/1110',
+            alt: '...',
+            ratio: 66,
+          },
+        ],
       },
-    ]
+    ],
   },
   {
     type: 'col',
     subset: [
       {
-        type: "img",
-        url: "https://via.placeholder.com/365x500",
-        alt: "..."
-      }
-    ]
+        type: 'img',
+        url: 'https://via.placeholder.com/365x500',
+        alt: '...',
+      },
+    ],
   },
   {
     type: 'col',
@@ -59,58 +60,71 @@ const imageTreeTest = [
         type: 'row',
         subset: [
           {
-            type: "img",
-            url: "https://via.placeholder.com/1500",
-            alt: "...",
-            ratio: 33
+            type: 'img',
+            url: 'https://via.placeholder.com/1500',
+            alt: '...',
+            ratio: 33,
           },
           {
-            type: "img",
-            url: "https://via.placeholder.com/1500",
-            alt: "...",
-            ratio: 66
-          }
-        ]
+            type: 'img',
+            url: 'https://via.placeholder.com/1500',
+            alt: '...',
+            ratio: 66,
+          },
+        ],
       },
       {
         type: 'row',
         subset: [
           {
-            type: "img",
-            url: "https://via.placeholder.com/1500",
-            alt: "...",
-            ratio: 2
+            type: 'img',
+            url: 'https://via.placeholder.com/1500',
+            alt: '...',
+            ratio: 2,
           },
           {
-            type: "img",
-            url: "https://via.placeholder.com/1500",
-            alt: "...",
-            ratio: 1
-          }
-        ]
+            type: 'img',
+            url: 'https://via.placeholder.com/1500',
+            alt: '...',
+            ratio: 1,
+          },
+        ],
       },
-    ]
-  }
+    ],
+  },
 ]
 
 const ImageGallery: React.FC = () => {
-  const ratioTotal = imageTreeTest.reduce((total, set) => total + set.ratio || 0, 0)
+  const ratioTotal = imageTreeTest.reduce(
+    (total, set) => total + set.ratio || 0,
+    0
+  )
   return (
     <S.Container>
       <S.ContainerCentered>
-      {imageTreeTest.map((set, key) => {
-        set.ratio = 100 / ratioTotal * set.ratio || 100 / imageTreeTest.length // Set even size if ratio is not defined
-        if (set.type === "row") {
-          return <S.Row ratio={set.ratio}><Item {...set} key={key} /></S.Row>
-        }
-        if (set.type === "col") {
-          return <S.Col ratio={set.ratio}><Item {...set} key={key} /></S.Col>
-        }
-        if (set.type === "img") {
-          return <Img {...set} key={key} />
-        }
-        return null // Unknown type - nothing will be rendered
-      })}
+        {imageTreeTest.map((set, key) => {
+          set.ratio =
+            (100 / ratioTotal) * set.ratio || 100 / imageTreeTest.length // Set even size if ratio is not defined
+          if (set.type === 'row') {
+            return (
+              <S.Row ratio={set.ratio}>
+                <Item {...set} key={key} />
+              </S.Row>
+            )
+          }
+          if (set.type === 'col') {
+            return (
+              <S.Col ratio={set.ratio}>
+                <Item {...set} key={key} />
+              </S.Col>
+            )
+          }
+          if (set.type === 'img') {
+            return <Img {...set} key={key} />
+          }
+
+          return null // Unknown type - nothing will be rendered
+        })}
       </S.ContainerCentered>
     </S.Container>
   )
