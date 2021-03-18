454e4d4933f9b699c6dc3d01706db1c31bc5cb09
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-03-18 21:51:18 +0100

    refactored form validation and submission

---
+++ src/components/layout/footer/newsletter-form/submit.ts
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { NewsletterFormValues } from '.'
+
+type SubmitFunction = (values: NewsletterFormValues) => Promise<void>
+
+export const useOnSubmitNewsletter = (): [SubmitFunction, boolean, boolean] => {
+  const [hasError, setHasError] = useState(false)
+  const [hasSubscribed, setHasSubscribed] = useState(false)
+
+  const submit: SubmitFunction = async (values) => {
+    setHasError(false)
+
+    try {
+      await fetch('/api/newsletter', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(values),
+      })
+      setHasSubscribed(true)
+    } catch (error) {
+      setHasError(true)
+    }
+  }
+
+  return [submit, hasError, hasSubscribed]
+}
