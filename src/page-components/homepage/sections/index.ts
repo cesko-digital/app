5fb646e9218dbbdcc93a15b7bb20bf0fae20a8a1
Tuan Phung <tuan@phung.cz>
2021-03-25 14:21:44 +0100

    added galery image

--- src/page-components/homepage/sections/index.ts
+++ src/page-components/homepage/sections/index.ts
@@ -1,3 +1,4 @@
 export { default as Hero } from './hero'
 export { default as OurValues } from './our-values'
 export { default as Numbers } from './numbers'
+export { default as ImageGallery } from './image-gallery/index'
