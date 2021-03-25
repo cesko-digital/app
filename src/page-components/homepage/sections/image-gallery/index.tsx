650f83858f4f95071351af9a1129b18e527aa28b
Tuan Phung <tuan@phung.cz>
2021-03-25 14:21:44 +0100

    added galery image

---
+++ src/page-components/homepage/sections/image-gallery/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import * as S from './styles'
+import galleryImageData from './galleryImageData.json'
+import Image from './image/index'
+import { GALLERY_IMAGE_POSITIONS } from './helpers'
+
+export interface ImageProps {
+  imageUrl: string
+  alt: string
+  id?: number
+  type?: string
+}
+
+export interface ImageGalleryPositions {
+  colStart: number
+  colEnd: number
+  rowStart: number
+  rowEnd: number
+  position: number
+}
+
+const ImageGallery: React.FC = () => (
+  <S.Container>
+    <S.Grid>
+      {galleryImageData.map((imageItem: ImageProps) => {
+        const positions = GALLERY_IMAGE_POSITIONS.find(
+          ({ position }: ImageGalleryPositions) => position === imageItem.id
+        )
+        return (
+          positions && (
+            <Image key={imageItem.id} {...imageItem} positions={positions} />
+          )
+        )
+      })}
+    </S.Grid>
+  </S.Container>
+)
+
+export default ImageGallery
