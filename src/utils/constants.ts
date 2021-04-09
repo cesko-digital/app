07760dc178373855658adaf39ed1de9310899ed8
Horm <horm@horm.codes>
2021-04-09 17:37:02 +0200

    Add media contact and logo link to footer

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,4 +2,5 @@ export const LINKS = {
   submitProject: 'https://forms.gle/yAxPSueBJgyffEGi8',
   joinUs: 'https://join.cesko.digital/',
   supportUs: 'https://www.darujme.cz/projekt/1203553',
+  logo: 'https://znacka.cesko.digital/',
 }
