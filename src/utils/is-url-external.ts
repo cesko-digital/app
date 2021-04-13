0400133df7ef554bb2ef83b7ce3d342d36cf7f23
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-04-13 09:05:06 +0200

    Recognize mailto: scheme as an external url, fixes #248

--- src/utils/is-url-external.ts
+++ src/utils/is-url-external.ts
@@ -1,5 +1,8 @@
-const EXTERNAL_URL_REGEXP = /https?:\/\/((?:[\w\d-]+\.)+[\w\d]{2,})/i
-
 export function isExternalURL(url: string): boolean {
-  return Boolean(EXTERNAL_URL_REGEXP.exec(url))
+  const lc = url.toLowerCase()
+  return (
+    lc.startsWith('http://') ||
+    lc.startsWith('https://') ||
+    lc.startsWith('mailto:')
+  )
 }
