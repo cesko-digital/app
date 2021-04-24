6ddbc8c0522308c736faf661067a31a4a0811a55
MarketaZ <m.zadrazilova@gmail.com>
2021-04-24 16:37:46 +0200

    Display projects in 3 columns on large screens

--- src/page-components/projects/sections/ongoing-projects/index.tsx
+++ src/page-components/projects/sections/ongoing-projects/index.tsx
@@ -14,9 +14,13 @@ interface Props {
 
 export const Container = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: repeat(3, 1fr);
   gap: ${({ theme }) => `${theme.space.md}px ${theme.space.md}px`};
   margin-top: ${({ theme }) => theme.space.md}px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
+    grid-template-columns: repeat(2, 1fr);
+  }
 
   @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
     grid-template-columns: 1fr;
