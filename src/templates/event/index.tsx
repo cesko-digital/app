efe73c50fc468f56e555fd07e0162132724174fa
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-06-16 15:48:35 +0200

    Fix linter complaints

--- src/templates/event/index.tsx
+++ src/templates/event/index.tsx
@@ -2,9 +2,7 @@ import React from 'react'
 import { Heading1 } from 'components/typography'
 
 const EventPage: React.FC = () => {
-  return (
-    <Heading1>Všecko bude!</Heading1>
-  )
+  return <Heading1>Všecko bude!</Heading1>
 }
 
 export default EventPage
