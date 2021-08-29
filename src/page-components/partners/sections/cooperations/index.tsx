0705a9a36929539a5d856ae34a27a049dc543680
Rostislav Klein <rostislav.klein@pipedrive.com>
2021-08-29 17:40:00 +0200

    financial partners textation

--- src/page-components/partners/sections/cooperations/index.tsx
+++ src/page-components/partners/sections/cooperations/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import { useTranslation } from 'gatsby-plugin-react-i18next'
 import { Section, SectionContent } from 'components/layout'
-import { Heading2 } from 'components/typography'
+import { Body, Heading2 } from 'components/typography'
 
 const CooperationsPartners: React.FC = () => {
   const { t } = useTranslation()
   return (
     <Section>
       <SectionContent>
-        <Heading2>{t('pages.partners.cooperations.heading')}</Heading2>
+        <Heading2>{t('pages.partners.cooperations.heading.title')}</Heading2>
+      </SectionContent>
+      <SectionContent>
+        <Body>{t('pages.partners.cooperations.heading.perex')}</Body>
       </SectionContent>
     </Section>
   )
--- src/pages/partners.tsx
+++ src/pages/partners.tsx
@@ -1,1 +1,15 @@
+import { graphql } from 'gatsby'
+
+export const query = graphql`
+  query Partners {
+    partners: allPartner {
+      nodes {
+        name
+        url
+        logoUrl
+      }
+    }
+  }
+`
+
 export { default } from 'page-components/partners'
