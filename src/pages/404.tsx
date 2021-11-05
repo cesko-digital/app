6245a0849064886c0ef9caa64246185fbc5bbf14
Karel Minarik <karel.minarik@gmail.com>
2021-11-05 18:24:35 +0100

    Update 404 page content to pass Cypress tests

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -7,7 +7,7 @@ const NotFoundPage: React.FC = () => (
   <Layout seo={{ title: '404 - Not Found' }}>
     <Section>
       <SectionContent>
-        <h1>NOT FOUND</h1>
+        <h1>404 NOT FOUND</h1>
         <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
       </SectionContent>
     </Section>
