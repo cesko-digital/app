b546da0e6e85bdcd999ac1b5d5340d8b07cfc10b
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-06 17:05:31 +0100

    Add basic code to generate project pages

---
+++ pages/projekty/index.tsx
@@ -0,0 +1,38 @@
+import type { NextPage, GetStaticProps } from "next";
+import { getAllProjects } from "../../lib/airtable-import";
+import { PortalProject } from "../../lib/portal-types";
+
+type PageProps = {
+  projects: PortalProject[];
+};
+
+const Page: NextPage<PageProps> = ({ projects }) => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        {projects.map((p) => (
+          <li>
+            <ProjectLink {...p} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const ProjectLink: React.FC<PortalProject> = (project) => {
+  const path = `/projekty/${project.slug}`;
+  return <a href={path}>{project.name}</a>;
+};
+
+export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
+  const apiKey = process.env.AIRTABLE_API_KEY as string;
+  return {
+    props: {
+      projects: await getAllProjects(apiKey),
+    },
+  };
+};
+
+export default Page;
