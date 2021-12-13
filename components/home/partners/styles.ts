1460dcad8223748007d762b388e0902984ec1488
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-13 15:04:01 +0100

    Load partners from Airtable and display them on the homepage

---
+++ components/home/partners/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Heading2 } from "components/typography";
+
+export const MainTitle = styled(Heading2)`
+  margin: 0 0 65px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.sm}) {
+    margin-bottom: 40px;
+  }
+`;
+
+export const Wrapper = styled.div`
+  margin: 0 0 150px;
+`;
