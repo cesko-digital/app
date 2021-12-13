32d4c72aa407b4ad13fd4dbf346c24f836fac6a4
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-13 11:08:25 +0100

    Add more utility functions

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,26 @@
 // This is a hack, see https://github.com/vercel/next.js/issues/11993
 export const prepareToSerialize = (data: any) =>
   JSON.parse(JSON.stringify(data));
+
+export function isExternalURL(url: string): boolean {
+  const lc = url.toLowerCase();
+  return (
+    lc.startsWith("http://") ||
+    lc.startsWith("https://") ||
+    lc.startsWith("mailto:")
+  );
+}
+
+export function getResizedImgUrl(
+  originalUrl: string,
+  targetWidth: number
+): string {
+  return originalUrl
+    ? originalUrl.replaceAll(
+        "data.cesko.digital",
+        "cesko.digital/api/resize?src="
+      ) +
+        "&width=" +
+        targetWidth
+    : "";
+}
