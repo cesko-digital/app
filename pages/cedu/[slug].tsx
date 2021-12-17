d4e61b7baaff27771cad2b03dc3e16ecce26bb02
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-17 11:01:03 +0100

    Add UI components for č.edu videos

--- pages/cedu/[slug].tsx
+++ pages/cedu/[slug].tsx
@@ -2,6 +2,23 @@ import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import { getAllVideos, PortalVideo } from "lib/cedu";
 import { prepareToSerialize } from "lib/utils";
 import { ParsedUrlQuery } from "querystring";
+import { useRouter } from "next/router";
+import { Layout, Section, SectionContent } from "components/layout";
+import * as Typography from "../../components/typography";
+import {
+  BoxesColumn,
+  MainColumn,
+  TwoColumnLayout,
+  VideoIframe,
+  VideoWrapper,
+} from "components/cedu/styles";
+import {
+  CreditsBox,
+  ResourceBox,
+  TableOfContentBox,
+} from "components/cedu/content-box";
+import { Route } from "lib/routing";
+import RenderMarkdown from "components/markdown";
 
 interface PageProps {
   video: PortalVideo;
@@ -12,7 +29,52 @@ interface QueryParams extends ParsedUrlQuery {
 }
 
 const Page: NextPage<PageProps> = ({ video }) => {
-  return <pre>{JSON.stringify(video, null, 2)}</pre>;
+  const router = useRouter();
+  const { start } = router.query;
+
+  return (
+    <Layout
+      crumbs={[
+        { path: Route.volunteerPortal, label: "Portál dobrovolníka" },
+        { label: video.title },
+      ]}
+      seo={{
+        title: video.title,
+        description: video.description,
+        coverUrl: video.cover,
+      }}
+    >
+      <Section>
+        <SectionContent>
+          <Typography.Heading1>{video.title}</Typography.Heading1>
+        </SectionContent>
+      </Section>
+      <Section>
+        <SectionContent>
+          <TwoColumnLayout>
+            <MainColumn>
+              <VideoWrapper>
+                <VideoIframe
+                  src={video.videoUrl + "?start=" + start}
+                  title={video.title}
+                  frameBorder="0"
+                  allowFullScreen
+                />
+              </VideoWrapper>
+              <Typography.Body>
+                <RenderMarkdown source={video.transcript} />
+              </Typography.Body>
+            </MainColumn>
+            <BoxesColumn>
+              <TableOfContentBox segments={video.toc} />
+              <ResourceBox resources={video.resources} />
+              <CreditsBox credits={video.credits} />
+            </BoxesColumn>
+          </TwoColumnLayout>
+        </SectionContent>
+      </Section>
+    </Layout>
+  );
 };
 
 export const getStaticPaths: GetStaticPaths<QueryParams> = async () => {
