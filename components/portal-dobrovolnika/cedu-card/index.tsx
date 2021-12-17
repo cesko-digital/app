6766ce83a4b5319a2c314d0cacf1a715eaa66e6e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-17 10:31:59 +0100

    Add č.edu videos to the Volunteer portal homepage

---
+++ components/portal-dobrovolnika/cedu-card/index.tsx
@@ -0,0 +1,39 @@
+import * as S from "./styles";
+import { Link } from "components/links";
+import { PortalVideo } from "lib/cedu";
+import strings from "content/strings.json";
+import { Route } from "lib/routing";
+
+interface Props {
+  video: PortalVideo;
+}
+
+const CeduCard: React.FC<Props> = ({ video, ...rest }) => {
+  return (
+    <S.Card {...rest}>
+      <S.Header>
+        <S.Cover
+          url={video.cover}
+          aria-label={`${strings.cards.project.coverAriaLabel} ${video.title}`}
+        />
+        <S.CoverWrap />
+        <S.Logo
+          url="https://data.cesko.digital/web/projects/cesko-digital/logo.png"
+          aria-label={`${strings.cards.project.logoAriaLabel} ${video.title}`}
+        />
+      </S.Header>
+      <S.Content>
+        <S.Title>{video.title}</S.Title>
+        <S.TagList>
+          {video.tags.map((tag) => (
+            <S.Tag key={tag}>#{tag}</S.Tag>
+          ))}
+        </S.TagList>
+        <S.Description>{video.description}</S.Description>
+        <Link to={Route.toVideo(video)}>To mě zajímá!</Link>
+      </S.Content>
+    </S.Card>
+  );
+};
+
+export default CeduCard;
