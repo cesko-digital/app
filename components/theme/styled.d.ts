a10c7b8b5010b8c1028e8d1de4496fb7f77d4185
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-12-17 07:57:39 +0100

    Simplify custom theme type

--- components/theme/styled.d.ts
+++ components/theme/styled.d.ts
@@ -1,103 +1,10 @@
 import "styled-components";
+import { defaultTheme } from "./default";
 
-// TBD: Could we do this using `typeof` instead of by hand?
+type DefaultThemeType = typeof defaultTheme;
+
+// This matches the DefaultTheme type from Styled Components to the type of our custom default theme
 declare module "styled-components" {
-  export interface DefaultTheme {
-    borderRadius: {
-      none: number;
-      base: number;
-      small: number;
-    };
-    borderWidth: {
-      none: number;
-      normal: number;
-    };
-    breakpoints: {
-      sm: string;
-      md: string;
-      lg: string;
-    };
-    animation: {
-      duration: {
-        base: string;
-      };
-    };
-    fontSizes: {
-      xxxl: number;
-      xxl: number;
-      xl: number;
-      l: number;
-      lg: number;
-      md: number;
-      base: number;
-      small: number;
-    };
-    colors: {
-      lightGray: string;
-      mediumGray: string;
-      darkGrey: string;
-      asphalt: string;
-      stone: string;
-      gravel: string;
-      pebble: string;
-      white: string;
-
-      it: string;
-      darkIt: string;
-      martinique: string;
-      purple: string;
-      violet: string;
-      lightViolet: string;
-
-      yellow: string;
-      orange: string;
-      darkRed: string;
-    };
-    contentSize: number;
-    space: {
-      xxxl: number;
-      xxl: number;
-      xl: number;
-      lg: number;
-      outer: number;
-      md: number;
-      base: number;
-      small: number;
-      none: number;
-    };
-    fonts: {
-      body: string;
-      heading: string;
-      monospace: string;
-    };
-    fontWeights: {
-      body: number;
-      heading: number;
-      bold: number;
-      button: number;
-    };
-    lineHeights: {
-      text: number;
-      body: number;
-      heading: number;
-      button: number;
-    };
-    shadows: {
-      small: string;
-      large: string;
-    };
-    variants: unknown;
-    text: unknown;
-    buttons: {
-      primary: {
-        color: string;
-        bg: string;
-      };
-    };
-    controlHeights: {
-      normal: number;
-    };
-  }
-
-  export type CssWithTheme = FlattenInterpolation<ThemeProps<DefaultTheme>>;
+  interface DefaultTheme extends DefaultThemeType {}
+  type CssWithTheme = FlattenInterpolation<ThemeProps<DefaultTheme>>;
 }
