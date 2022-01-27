4272e8b00bbb124ea4f0a7486ca0b15ec4219813
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-01-27 09:58:56 +0100

    Only insert analytics tracking code in production builds

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { defaultTheme } from "components/theme/default";
 import Script from "next/script";
+import { env } from "lib/env";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -16,15 +17,17 @@ const GlobalStyle = createGlobalStyle`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
+      {env.allowRobots && (
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer', 'GTM-PRMTS8P');
       `}
-      </Script>
+        </Script>
+      )}
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
