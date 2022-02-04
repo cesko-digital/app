793e28851fdcb06c14caa1528d5af0386d536934
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-04 18:20:19 +0100

    Try turning off Google Analytics cookies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -25,6 +25,17 @@ function MyApp({ Component, pageProps }: AppProps) {
         })(window,document,'script','dataLayer', 'GTM-PRMTS8P');
       `}
       </Script>
+      <Script id="google-tag-manager-consent" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag() { dataLayer.push(arguments); }
+        gtag('consent', 'default', {
+          'ad_storage': 'denied',
+          'analytics_storage': 'denied',
+          'personalization_storage': 'denied'
+        });
+      `}
+      </Script>
       <GlobalStyle />
       <Component {...pageProps} />
     </ThemeProvider>
