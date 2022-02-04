ee1fd6b57a1b199034e568d055048b4cc3f3255e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-04 19:07:10 +0100

    Revert "Remove all non-functional cookies; add Plausible Analytics (#486)"
    This reverts commit c375e70d2b4c0d15e08275bf4b74a159e6221a40. It looks
    like the change broke Google Analytics logging, so let’t back off and
    try again later.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -16,24 +16,13 @@ const GlobalStyle = createGlobalStyle`
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Script id="google-tag-manager-consent" strategy="afterInteractive">
-        {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag() { dataLayer.push(arguments); }
-        gtag('consent', 'default', {
-          'ad_storage': 'denied',
-          'analytics_storage': 'denied',
-          'personalization_storage': 'denied'
-        });
-      `}
-      </Script>
       <Script id="google-tag-manager" strategy="afterInteractive">
         {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', 'GTM-PRMTS8P');
+        })(window,document,'script','dataLayer', 'GTM-PWGVF79');
       `}
       </Script>
       <GlobalStyle />
