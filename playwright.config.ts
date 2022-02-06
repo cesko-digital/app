6a123eef57322a736fa3ddd7edf83be2ddb2f4a5
Tom Burger <Tburger@glaass.net>
2022-02-06 22:23:18 +0100

    config file simplified

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,18 +3,12 @@ import path from 'path'
 
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
-  // Timeout per test
   timeout: 30 * 1000,
-  // Test directory
   testDir: path.join(__dirname, 'tests'),
   testMatch: 'e2e/**/*.spec.ts',
-  // If a test fails, retry it additional 2 times
-  retries: 2,
-  // Artifacts folder where screenshots, videos, and traces are stored.
+  retries: 0,
   outputDir: 'tests/results/',
 
-  // Run your local dev server before starting the tests:
-  // https://playwright.dev/docs/test-advanced#launching-a-development-web-server-during-the-tests
   webServer: {
     command: 'yarn dev',
     port: 3000,
@@ -25,46 +19,7 @@ const config: PlaywrightTestConfig = {
   use: {
     baseURL: 'http://localhost:3000',
     headless: true,
-    // Retry a test if its failing with enabled tracing. This allows you to analyse the DOM, console logs, network traffic etc.
-    // More information: https://playwright.dev/docs/trace-viewer
-    trace: 'retry-with-trace',
-
-    // All available context options: https://playwright.dev/docs/api/class-browser#browser-new-context
-    // contextOptions: {
-    //   ignoreHTTPSErrors: true,
-    // },
+    browserName: 'chromium',
   },
-
-  projects: [
-    {
-      name: 'Desktop Chrome',
-      use: {
-        ...devices['Desktop Chrome'],
-      }
-    },
-    // {
-    //   name: 'Desktop Firefox',
-    //   use: {
-    //     ...devices['Desktop Firefox'],
-    //   },
-    // },
-    // {
-    //   name: 'Desktop Safari',
-    //   use: {
-    //     ...devices['Desktop Safari'],
-    //   },
-    // },
-    // Test against mobile viewports.
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: devices['iPhone 12'],
-    // },
-  ],
 }
 export default config
