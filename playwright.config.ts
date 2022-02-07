04e85bcb49481147bddbb1bc55c772fbc6783b95
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-07 09:52:49 +0100

    Format Playwright config using Prettier

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,25 +1,26 @@
-import { PlaywrightTestConfig, devices } from '@playwright/test'
-import path from 'path'
+import { PlaywrightTestConfig } from "@playwright/test";
+import path from "path";
 
 // Reference: https://playwright.dev/docs/test-configuration
 const config: PlaywrightTestConfig = {
   timeout: 30 * 1000,
-  testDir: path.join(__dirname, 'tests'),
-  testMatch: 'e2e/**/*.spec.ts',
+  testDir: path.join(__dirname, "tests"),
+  testMatch: "e2e/**/*.spec.ts",
   retries: 0,
-  outputDir: 'tests/results/',
+  outputDir: "tests/results/",
 
   webServer: {
-    command: 'yarn dev',
+    command: "yarn dev",
     port: 3000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 
   use: {
-    baseURL: 'http://localhost:3000',
+    baseURL: "http://localhost:3000",
     headless: true,
-    browserName: 'chromium',
+    browserName: "chromium",
   },
-}
-export default config
+};
+
+export default config;
