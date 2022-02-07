8a3223cbd5ff6c295786305d5191ddf760ea869d
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-07 11:04:07 +0100

    Bypass draft data filter for the moment

--- lib/site-data.ts
+++ lib/site-data.ts
@@ -39,8 +39,10 @@ async function loadSiteData(): Promise<SiteData> {
   let opportunities = await DataSource.getAllOpportunities();
   let events = await DataSource.getAllEvents();
 
-  // Filter out draft data
-  if (!env.includeDraftData) {
+  // Filter out draft data. This is intentionally bypassed
+  // at the moment, since we still rely on draft projects being
+  // sort-of published to production. TBD.
+  if (false) {
     projects = projects.filter((p) => p.state !== "draft");
     opportunities = opportunities.filter((o) => o.status !== "draft");
     events = events.filter((e) => e.status !== "draft");
