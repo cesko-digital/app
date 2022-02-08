99aee6713b7c03630edc94eea3d17d5f8a16eecd
Tom Burger <Tburger@glaass.net>
2022-02-08 19:32:47 +0100

    allow tags to be null

--- pages/partners.tsx
+++ pages/partners.tsx
@@ -69,7 +69,7 @@ const Page: NextPage<PageProps> = ({ partners, blogPosts }) => {
 export const getStaticProps: GetStaticProps<PageProps> = async () => {
   const partners = siteData.partners;
   const blogPosts = siteData.blogPosts
-    .filter((post) => post.tags.some((t) => t === "partners"))
+    .filter((post) => post.tags && post.tags.some((t) => t === "partners"))
     .reverse()
     .slice(0, 3);
   return {
