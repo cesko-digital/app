9e4f34af4a8142dcc2399fce81de7cb6dc2aa07a
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-10 08:16:14 +0100

    Cache Markdown opportunity export

--- pages/api/opportunities.md.ts
+++ pages/api/opportunities.md.ts
@@ -10,7 +10,12 @@ export default async function handler(
   const opportunities = siteData.opportunities.filter(
     (o) => o.status === "live"
   );
+  const oneHour = 60 * 60;
   const mdown = renderOpportunitiesBySkill(opportunities, projects, users);
+  response.setHeader(
+    "Cache-Control",
+    `max-age=${oneHour}, s-maxage=${oneHour}`
+  );
   response.setHeader("Content-Type", "text/markdown; charset=UTF-8");
   response.status(200).send(mdown.source);
 }
