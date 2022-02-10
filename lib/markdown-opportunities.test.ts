fb222b39c1fb6c0e584d62065b4bd77011760f1e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-10 08:54:48 +0100

    Add API endpoint to export opportunities in Markdown (#480)
    * Add first draft code to render opportunities to Markdown
    
    * Add API endpoint for opportunities in Markdown
    
    * Explicitly set response encoding for Markdown opportunities
    
    * Sort opportunities in Markdown export by skill
    
    * Cache Markdown opportunity export
    
    * Add subject to mailto: link in opportunity Markdown export
    
    * Add utm_medium for tracking newsletter links to opportunities

---
+++ lib/markdown-opportunities.test.ts
@@ -0,0 +1,30 @@
+import { renderList, renderOpportunity } from "./markdown-opportunities";
+import { MarkdownString } from "./utils";
+
+describe("Render opportunities to Markdown", () => {
+  test("Render single opportunity", () => {
+    expect(
+      renderOpportunity(
+        {
+          name: "Scrum Master",
+          slug: "scrum-master",
+        },
+        {
+          name: "Pohyb je řešení",
+        },
+        {
+          name: "Anežka Müller",
+          email: "anezka@cesko.digital",
+        }
+      )
+    ).toEqual<MarkdownString>({
+      source:
+        "**[Scrum Master](https://cesko.digital/opportunities/scrum-master?utm_medium=newsletter)** pro projekt Pohyb je řešení, kontaktní osoba [Anežka Müller](mailto:anezka@cesko.digital?subject=Scrum%20Master)",
+    });
+  });
+  test("Render list", () => {
+    expect(renderList(["foo", "bar"], (item) => ({ source: item }))).toEqual({
+      source: "* foo\n* bar",
+    });
+  });
+});
