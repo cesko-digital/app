7622f1fdac4f5fd615d6dea9b63ecb843ae51476
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-18 11:07:01 +0100

    Move Google Tag Manager code from _app.tsx to Layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -29,6 +29,15 @@ const Layout: React.FC<Props> = ({
         data-domain="cesko.digital"
         src="https://plausible.io/js/plausible.js"
       />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', 'GTM-PRMTS8P');
+      `}
+      </Script>
       <CustomHead {...seo} />
       {showBanner && <Banner />}
       {lang === "cs" && <HeaderCS />}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,11 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "components/theme/default";
-import Script from "next/script";
 import "components/global.css";
 
-// This is here only because of Styled Components. If we manage to find some other
-// styling solution, it should be possible to simplify this whole file.
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', 'GTM-PRMTS8P');
-      `}
-      </Script>
       <Component {...pageProps} />
     </ThemeProvider>
   );
