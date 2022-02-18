1a4999e40218581bf81f1b80ae5c19d65edb5ff4
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-02-18 11:09:30 +0100

    Remove unnecessary strategy="afterInteractive"
    It’s the default behaviour, no need to specify it explicitly.

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -29,7 +29,7 @@ const Layout: React.FC<Props> = ({
         data-domain="cesko.digital"
         src="https://plausible.io/js/plausible.js"
       />
-      <Script id="google-tag-manager" strategy="afterInteractive">
+      <Script id="google-tag-manager">
         {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
