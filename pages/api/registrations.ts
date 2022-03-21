9d20900afb079041ba7ef3923ee8367933313547
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-03-21 15:01:16 +0100

    Refuse to create new registrations for existing e-mails

--- pages/api/registrations.ts
+++ pages/api/registrations.ts
@@ -6,6 +6,7 @@ export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  // Validate input
   const { name, email, skills } = request.body;
   if (!name) {
     response.status(400).send("Missing “name” param.");
@@ -21,7 +22,23 @@ export default async function handler(
   }
   try {
     const apiKey = process.env.AIRTABLE_API_KEY;
-    const base = new Airtable({ apiKey }).base("apppZX1QC3fl1RTBM");
+    const base = new Airtable({ apiKey }).base("apppZX1QC3fl1RTBM")(
+      "Profiles 2.0"
+    );
+
+    // Make sure the email doesn’t exist already
+    const previousRecords = await base
+      .select({
+        filterByFormula: `{email} = "${email}"`,
+      })
+      .all();
+    if (previousRecords.length != 0) {
+      const msg = "Email already exists";
+      console.error(msg);
+      response.status(401).send(msg);
+      return;
+    }
+
     const state = "unconfirmed";
     const registration = {
       fields: {
@@ -31,9 +48,10 @@ export default async function handler(
         state,
       },
     };
-    await base("Profiles 2.0").create([registration]);
+    await base.create([registration]);
     response.status(201).send("Registration created.");
   } catch (e) {
+    console.error(e);
     response.status(500).send("Sorry :(");
   }
 }
