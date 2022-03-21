e9a5e6b94737571a772d3133b1fb45f99d6b33b5
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-03-21 15:03:58 +0100

    Move onboarding form to new DB table (#539)
    * Move the onboarding form to the new Profiles 2.0 table
    
    * Refuse to create new registrations for existing e-mails

--- pages/api/registrations.ts
+++ pages/api/registrations.ts
@@ -1,10 +1,12 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from "next";
 import Airtable from "airtable";
 
+/** Create a new, unconfirmed user profile */
 export default async function handler(
   request: NextApiRequest,
   response: NextApiResponse
 ) {
+  // Validate input
   const { name, email, skills } = request.body;
   if (!name) {
     response.status(400).send("Missing “name” param.");
@@ -20,17 +22,36 @@ export default async function handler(
   }
   try {
     const apiKey = process.env.AIRTABLE_API_KEY;
-    const base = new Airtable({ apiKey }).base("apppZX1QC3fl1RTBM");
+    const base = new Airtable({ apiKey }).base("apppZX1QC3fl1RTBM")(
+      "Profiles 2.0"
+    );
+
+    // Make sure the email doesn’t exist already
+    const previousRecords = await base
+      .select({
+        filterByFormula: `{email} = "${email}"`,
+      })
+      .all();
+    if (previousRecords.length != 0) {
+      const msg = "Email already exists";
+      console.error(msg);
+      response.status(401).send(msg);
+      return;
+    }
+
+    const state = "unconfirmed";
     const registration = {
       fields: {
-        Name: name,
-        Email: email,
-        Skills: skills,
+        name,
+        email,
+        skills,
+        state,
       },
     };
-    await base("Registrations").create([registration]);
+    await base.create([registration]);
     response.status(201).send("Registration created.");
   } catch (e) {
+    console.error(e);
     response.status(500).send("Sorry :(");
   }
 }
