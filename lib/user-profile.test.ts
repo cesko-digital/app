b545ca55300c7e79291ce19af0afcee3b33c33e2
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-03-23 12:06:21 +0100

    Improve Airtable access, test user profile endpoint

---
+++ lib/user-profile.test.ts
@@ -0,0 +1,41 @@
+import { getUserProfile, UserProfile } from "./user-profile";
+
+test("Encode request", () => {
+  expect(getUserProfile("slack-id")).toEqual({
+    method: "SELECT",
+    params: {
+      filterByFormula: `{slackId} = "slack-id"`,
+    },
+    decodeResponse: expect.any(Function),
+  });
+});
+
+test("Decode response", () => {
+  const decoder = getUserProfile("slack-id").decodeResponse;
+  expect(decoder([])).toBeUndefined();
+  expect(
+    decoder([
+      {
+        id: "uisoh7Ei",
+        fields: {
+          name: "John Smith",
+          email: "john@smith.name",
+          skills: ["foo", "bar"],
+          slackId: "slack-id",
+          state: "confirmed",
+          createdAt: "2022-03-23T08:31:54.500Z",
+          lastModifiedAt: "2022-03-23T08:31:54.500Z",
+        },
+      },
+    ])
+  ).toEqual<UserProfile>({
+    id: "uisoh7Ei",
+    name: "John Smith",
+    email: "john@smith.name",
+    skills: ["foo", "bar"],
+    slackId: "slack-id",
+    state: "confirmed",
+    createdAt: "2022-03-23T08:31:54.500Z",
+    lastModifiedAt: "2022-03-23T08:31:54.500Z",
+  });
+});
