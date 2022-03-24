197bb21a20edd23fe932a07b4e9ca8bfc16483d1
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-03-24 09:41:19 +0100

    Confirm user account when user signs in to Slack (#550)
    * Add API endpoint to handle Slack team_join events
    * Improve Slack event parsing, add team_join event handler
    * End response after receiving a team_join event
    * Decode user payload from team_join events
    * Improve Slack user profile decoding
    * Confirm account in DB when user joins Slack
    * Add Slack event verification
    * Trivial refactoring
    * Add code comments
    * Improve Slack team_join event validation
    * Read user email from Slack API when confirming user account
    * Add missing “await”

---
+++ lib/slack/user.ts
@@ -0,0 +1,79 @@
+import {
+  boolean,
+  decodeType,
+  optional,
+  record,
+  string,
+} from "typescript-json-decoder";
+
+/** Slack user profile with information such as read name or e-mail */
+export type SlackProfile = decodeType<typeof decodeSlackProfile>;
+export const decodeSlackProfile = record({
+  real_name: optional(string),
+  display_name: string,
+  email: optional(string),
+});
+
+/** Slack user with information such as user ID, team ID or profile */
+export type SlackUser = decodeType<typeof decodeSlackUser>;
+export const decodeSlackUser = record({
+  id: string,
+  team_id: string,
+  name: string,
+  real_name: optional(string),
+  is_bot: boolean,
+  is_email_confirmed: optional(boolean),
+  profile: decodeSlackProfile,
+});
+
+/** Sample Slack profile to use in tests */
+export const sampleProfilePayload = {
+  title: "",
+  phone: "",
+  skype: "",
+  real_name: "Tomáš Znamenáček",
+  real_name_normalized: "Tomas Znamenacek",
+  display_name: "Tomáš Znamenáček",
+  display_name_normalized: "Tomas Znamenacek",
+  fields: null,
+  status_text: "",
+  status_emoji: "",
+  status_emoji_display_info: [],
+  status_expiration: 0,
+  avatar_hash: "g64fe32e779a",
+  first_name: "Tomáš",
+  last_name: "Znamenáček",
+  image_24: "https://…24.png",
+  image_32: "https://…32.png",
+  image_48: "https://…48.png",
+  image_72: "https://…72.png",
+  image_192: "https://…192.png",
+  image_512: "https://…512.png",
+  status_text_canonical: "",
+  team: "TG21XF887",
+};
+
+/** Sample Slack user to use in tests */
+export const sampleUserPayload = {
+  id: "U038G4SGK9Q",
+  team_id: "TG21XF887",
+  name: "zoul_test",
+  deleted: false,
+  color: "9b3b45",
+  real_name: "Tomáš Znamenáček",
+  tz: "Europe/Belgrade",
+  tz_label: "Central European Time",
+  tz_offset: 3600,
+  profile: sampleProfilePayload,
+  is_admin: false,
+  is_owner: false,
+  is_primary_owner: false,
+  is_restricted: false,
+  is_ultra_restricted: false,
+  is_bot: false,
+  is_app_user: false,
+  updated: 1648114412,
+  is_email_confirmed: true,
+  who_can_share_contact_card: "EVERYONE",
+  presence: "away",
+};
