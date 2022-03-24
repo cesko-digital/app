166df468b70a4ff047cce5d4a2502ae5d7003e46
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-03-24 14:12:14 +0100

    Confirm account in DB when user joins Slack

--- pages/api/slack_events/team_join.ts
+++ pages/api/slack_events/team_join.ts
@@ -1,5 +1,12 @@
+import Airtable from "airtable";
+import { send } from "lib/airtable-request";
 import { decodeIncomingMessage } from "lib/slack/events";
 import { NextApiRequest, NextApiResponse } from "next";
+import {
+  getUserProfileByMail,
+  updateUserProfile,
+  userProfileTable,
+} from "lib/user-profile";
 
 export default async function handler(
   request: NextApiRequest,
@@ -8,11 +15,25 @@ export default async function handler(
   try {
     const msg = decodeIncomingMessage(request.body);
     switch (msg.type) {
+      // This is just Slack making sure we own the endpoint
       case "url_verification":
         response.status(200).send(msg.challenge);
         return;
+      // This is Slack telling us we have a new user. Now we need to find
+      // the yet-unconfirmed account in the DB by mail and confirm it + add
+      // the Slack user ID.
       case "event_callback":
-        console.log(JSON.stringify(request.body, null, 2));
+        const {
+          id,
+          profile: { email },
+        } = msg.event.user;
+        if (email) {
+          await confirmUserAccount(id, email);
+        } else {
+          console.error(
+            `Email field in Slack profile empty when trying to confirm user “${id}”`
+          );
+        }
         response.status(204).end();
         return;
     }
@@ -20,4 +41,34 @@ export default async function handler(
     console.log(e);
     response.status(400).send("Don’t understand");
   }
+}
+
+async function confirmUserAccount(slackId: string, email: string) {
+  const apiKey = process.env.AIRTABLE_API_KEY;
+  const base = new Airtable({ apiKey }).base("apppZX1QC3fl1RTBM");
+  const table = userProfileTable(base);
+
+  const initialProfile = await send(table, getUserProfileByMail(email));
+
+  // This can routinely happen if the user skipped onboarding somehow
+  if (!initialProfile) {
+    console.error(
+      `Unconfirmed account not found when trying to confirm user “${slackId}” (${email})`
+    );
+    return;
+  }
+
+  // Not sure how this could happen, but let’s keep the diagnostics tight here
+  if (initialProfile.state === "confirmed" && initialProfile.slackId) {
+    console.warn(`Account “${slackId}” already confirmed, skipping`);
+    return;
+  }
+
+  await send(
+    table,
+    updateUserProfile(initialProfile.id, {
+      slackId,
+      state: "confirmed",
+    })
+  );
 }
