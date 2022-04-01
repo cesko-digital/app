01638ada1517aea19ae1995b9aa172fcae944a75
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-01 15:51:43 +0200

    Edit user skills on user profile page (#558)
    * Fix decoding for users with no skills
    
    * Make skills editable on user profile page
    
    * Add tiny skill-pill animation
    
    * Highlight fields with checked skills

--- lib/user-profile.test.ts
+++ lib/user-profile.test.ts
@@ -1,4 +1,4 @@
-import { getUserProfile, UserProfile } from "./user-profile";
+import { decodeUserProfile, getUserProfile, UserProfile } from "./user-profile";
 
 test("Encode request", () => {
   expect(getUserProfile("slack-id")).toEqual({
@@ -34,6 +34,30 @@ test("Decode response", () => {
     name: "John Smith",
     email: "john@smith.name",
     skills: ["foo", "bar"],
+    slackId: "slack-id",
+    state: "confirmed",
+    createdAt: "2022-03-23T08:31:54.500Z",
+    lastModifiedAt: "2022-03-23T08:31:54.500Z",
+  });
+});
+
+test("Decode user with no skills", () => {
+  expect(
+    decodeUserProfile({
+      id: "uisoh7Ei",
+      name: "John Smith",
+      email: "john@smith.name",
+      skills: undefined,
+      slackId: "slack-id",
+      state: "confirmed",
+      createdAt: "2022-03-23T08:31:54.500Z",
+      lastModifiedAt: "2022-03-23T08:31:54.500Z",
+    })
+  ).toEqual<UserProfile>({
+    id: "uisoh7Ei",
+    name: "John Smith",
+    email: "john@smith.name",
+    skills: [],
     slackId: "slack-id",
     state: "confirmed",
     createdAt: "2022-03-23T08:31:54.500Z",
