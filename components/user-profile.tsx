88a03d7a0dec05b20838890c3d1c00df8ad24a15
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-01 15:51:43 +0200

    Edit user skills on user profile page (#558)
    * Fix decoding for users with no skills
    
    * Make skills editable on user profile page
    
    * Add tiny skill-pill animation
    
    * Highlight fields with checked skills

--- components/user-profile.tsx
+++ components/user-profile.tsx
@@ -1,8 +1,11 @@
 import { UserProfile } from "lib/user-profile";
 import { Layout, Section, SectionContent } from "components/layout";
-import { Body, Heading1 } from "./typography";
+import { Body, Heading1, Heading2 } from "./typography";
 import { Button } from "./buttons";
 import { Skill } from "lib/skills";
+import { unique } from "lib/utils";
+import { useState } from "react";
+import styled from "styled-components";
 
 export type UserProfilePageState =
   | "loading"
@@ -11,11 +14,18 @@ export type UserProfilePageState =
   | "signed_in";
 
 export type UserProfilePageProps = {
-  userSkills: Skill[];
+  /** Page state (loading, signed out, …) */
   state: UserProfilePageState;
+  /** All available skills */
+  allSkills: Skill[];
+  /** User profile for the signed-in user */
   profile?: UserProfile;
+  /** What should be done when user clicks “Sign In” */
   signIn: () => void;
+  /** What should be done when user clicks “Sign Out” */
   signOut: () => void;
+  /** Called when selected skills change */
+  onUserSkillsChange: (skills: Skill[]) => void;
 };
 
 export const UserProfilePage: React.FC<UserProfilePageProps> = (props) => {
@@ -77,43 +87,130 @@ const ErrorPage = () => (
 
 const SignedInPage: React.FC<UserProfilePageProps> = (props) => {
   const profile = props.profile!;
-  const { signOut, userSkills } = props;
+  const { signOut, allSkills, onUserSkillsChange } = props;
   return (
     <MainContainer>
       <Heading>{profile.name}</Heading>
-      <SkillBox skills={userSkills} />
-      <Button onClick={signOut} style={{ marginTop: "40px" }} inverted>
+      <SkillBox
+        userSkillIds={profile.skills}
+        allSkills={allSkills}
+        onChange={onUserSkillsChange}
+      />
+      <Button
+        onClick={signOut}
+        style={{ marginTop: "40px", marginBottom: "40px" }}
+        inverted
+      >
         Odhlásit
       </Button>
     </MainContainer>
   );
 };
 
-const SkillBox: React.FC<{ skills: Skill[] }> = ({ skills }) => {
-  const haveSkills = skills.length > 0;
+type SkillBoxProps = {
+  /** All available skills */
+  allSkills: Skill[];
+  /** IDs of the skills selected by user */
+  userSkillIds: string[];
+  /** Called when selected skills change */
+  onChange: (skills: Skill[]) => void;
+};
+
+const SkillBox: React.FC<SkillBoxProps> = ({
+  userSkillIds,
+  allSkills,
+  onChange,
+}) => {
+  const userSkills = allSkills.filter((skill) =>
+    userSkillIds.includes(skill.id)
+  );
+  const [selectedSkills, setSelectedSkills] = useState(userSkills);
+  const fields = unique(allSkills.map((skill) => skill.field));
+
+  const toggleSkill = (skill: Skill) => {
+    const newSkills = selectedSkills.includes(skill)
+      ? selectedSkills.filter((other) => other.id !== skill.id)
+      : [skill, ...selectedSkills];
+    setSelectedSkills(newSkills);
+    onChange(newSkills);
+  };
+
+  const skillsForField = (field: string) =>
+    allSkills
+      .filter((skill) => skill.field === field)
+      .sort((a, b) => a.name.localeCompare(b.name));
+
+  const hasSelectedSkills = (field: string) =>
+    skillsForField(field).some((skill) => selectedSkills.includes(skill));
+
   return (
     <div style={{ marginBottom: "20px" }}>
-      {haveSkills &&
-        skills.map((skill, index) => <SkillPill key={index} {...skill} />)}
-      {!haveSkills && (
-        <Body>V uživatelském profilu nemáte vyplněné žádné dovednosti.</Body>
-      )}
+      <Body style={{ marginBottom: "20px" }}>
+        Co byste chtěli v Česko.Digital dělat? Dejte nám to vědět, ať vám můžeme
+        různými kanály nabízet relevantnější příležitosti. <i>(Copy TBD)</i>
+      </Body>
+
+      {fields.map((field) => (
+        <div key={field}>
+          <Heading2
+            style={{
+              marginBottom: "20px",
+              display: "inline-block",
+              background: hasSelectedSkills(field) ? "yellow" : "inherit",
+            }}
+          >
+            {field}
+          </Heading2>
+          <div style={{ lineHeight: "3ex", marginBottom: "20px" }}>
+            {skillsForField(field).map((skill) => (
+              <SkillPill
+                key={skill.id}
+                skill={skill}
+                onClick={toggleSkill}
+                checked={selectedSkills.some((s) => s.id === skill.id)}
+              />
+            ))}
+          </div>
+        </div>
+      ))}
     </div>
   );
 };
 
-const SkillPill = (skill: Skill) => (
-  <span
-    style={{
-      background: "#eee",
-      padding: "1ex 2ex 1ex 2ex",
-      borderRadius: "8px",
-      marginRight: "2ex",
-    }}
-  >
-    {skill.field.toLocaleLowerCase()} / {skill.name.toLocaleLowerCase()}
-  </span>
-);
+type SkillPillProps = {
+  skill: Skill;
+  checked: boolean;
+  onClick?: (skill: Skill) => void;
+};
+
+const SkillPill: React.FC<SkillPillProps> = ({
+  skill,
+  checked: checkedInitially,
+  onClick = (sender: Skill) => {},
+}) => {
+  const [checked, setChecked] = useState(checkedInitially);
+  const toggle = () => {
+    setChecked(!checked);
+    onClick(skill);
+  };
+  return (
+    <AnimatedSpan
+      onClick={toggle}
+      style={{
+        display: "inline-block",
+        background: checked ? "blue" : "#eee",
+        color: checked ? "white" : "black",
+        padding: "1ex 2ex 1ex 2ex",
+        borderRadius: "8px",
+        marginRight: "2ex",
+        marginBottom: "2ex",
+        cursor: "pointer",
+      }}
+    >
+      {skill.name}
+    </AnimatedSpan>
+  );
+};
 
 // Shared components
 
@@ -124,3 +221,10 @@ const MainContainer: React.FC = (props) => (
 const Heading: React.FC = (props) => (
   <Heading1 style={{ marginBottom: "20px" }}>{props.children}</Heading1>
 );
+
+const AnimatedSpan = styled.span`
+  transition: transform 0.1s;
+  &:active {
+    transform: scale(0.9);
+  }
+`;
