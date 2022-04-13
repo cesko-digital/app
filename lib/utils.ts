38d75083d4da9e6c415d2504431957391267a8cc
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-13 12:52:31 +0200

    Sync users from Slack to Airtable (#562)
    * Add basic code to query Slack workspace users
    
    * Trivial refactoring
    
    * Add support for creating records to Airtable client code
    
    * Add support for user profile images to Slack user type
    
    * Add batched CREATE and UPDATE Airtable requests
    
    * Save Slack users to Airtable
    
    * Add documentation to utils.ts
    
    * Add documentation to slack-user.ts
    
    * Move update-local-data script to bin/
    
    * Add GitHub Action to sync Slack users daily
    
    * Add more debug logging to the sync-users script
    
    * Remove useless try/catch block
    
    * Fix sync Action schedule
    
    * Add Airtable API key to sync-users Action

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,10 +3,12 @@ import { marked } from "marked";
 /** Our Google Analytics tracking ID */
 export const analyticsId = "UA-140227366-1";
 
+/** A simple string wrapper to avoid bugs from mixing HTML strings and Markdown source */
 export type MarkdownString = {
   source: string;
 };
 
+/** Convert Markdown string to HTML */
 export function markdownToHTML(source: string): string {
   return marked.parse(source, {
     breaks: true,
@@ -34,6 +36,7 @@ export function isOwnerEmailDisplayed(input: string): boolean {
   return /^anezka@cesko.digital|^gabriela@cesko.digital/.test(input);
 }
 
+/** Shuffle array in place, returns a reference to the same array */
 export function shuffleInPlace<T>(array: T[]): T[] {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -42,7 +45,22 @@ export function shuffleInPlace<T>(array: T[]): T[] {
   return array;
 }
 
+/** Split array to chunks of size `size` */
+export function splitToChunks<T>(array: T[], size: number) {
+  let chunks = [];
+  let i = 0;
+  let n = array.length;
+
+  while (i < n) {
+    chunks.push(array.slice(i, (i += size)));
+  }
+
+  return chunks;
+}
+
+/** Filter out duplicates from an array, returns a new array */
 export const unique = <T>(a: T[]) => [...new Set(a)];
 
+/** Return a random element from an array */
 export const getRandomElem = <T>(a: T[]) =>
   a[Math.floor(Math.random() * a.length)];
