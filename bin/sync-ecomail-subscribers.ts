e8fe8c7d0c182d77c7a0ce1769cbd830e0449105
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-22 10:24:38 +0200

    Sync users to Ecomail (#570)
    * Add function to get all user profiles from DB
    
    * Add script to bulk-subscribe users to Ecomail
    
    * Rename Slack sync script

---
+++ bin/sync-ecomail-subscribers.ts
@@ -0,0 +1,23 @@
+require("dotenv").config({ path: ".env.local" });
+
+import { getAllUserProfiles } from "lib/airtable/user-profile";
+import { addSubscribers } from "lib/ecomail";
+
+async function main() {
+  console.log("Downloading all user profiles.");
+  const userProfiles = await getAllUserProfiles();
+  const confirmedUsers = userProfiles.filter(
+    (user) => user.state === "confirmed"
+  );
+  console.log(
+    `Successfully downloaded and parsed ${userProfiles.length} user profiles, ${confirmedUsers.length} confirmed.`
+  );
+
+  console.log(`Uploading ${confirmedUsers.length} addresses to Ecomail.`);
+  await addSubscribers(
+    process.env.ECOMAIL_API_KEY || "",
+    confirmedUsers.map((user) => user.email)
+  );
+}
+
+main().catch((e) => console.error(e));
