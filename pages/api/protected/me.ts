8d87bf86f48cc83e8ebb1a43c6daa8150d7b9fd0
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-22 10:21:32 +0200

    Create user profile during first sign-in (#571)
    * Create user profile on first login attempt
    
    * Add linked Slack User record when creating user profile

--- pages/api/protected/me.ts
+++ pages/api/protected/me.ts
@@ -1,13 +1,23 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getToken } from "next-auth/jwt";
 import { assert } from "console";
-import { getUserProfile, updateUserProfile } from "lib/airtable/user-profile";
+import {
+  createUserProfile,
+  getUserProfile,
+  updateUserProfile,
+} from "lib/airtable/user-profile";
+import { getSlackUserBySlackId } from "lib/airtable/slack-user";
 
 /**
  * Retrieve or update user profile
  *
- * BEWARE: All we can trust for user identification is the JWT
+ * **BEWARE:** All we can trust for user identification is the JWT
  * session token. Anything else may be spoofed by the user.
+ *
+ * If the user is trying to GET a user profile that does not exist yet,
+ * we may safely create the confirmed profile on the fly, since everything
+ * that’s needed to create and confirm the profile is already in the
+ * session token.
  */
 export default async function handler(
   request: NextApiRequest,
@@ -19,23 +29,33 @@ export default async function handler(
     return;
   }
 
-  assert(token.sub, "Slack ID missing in JWT token.");
+  const slackId = token.sub!;
+  assert(slackId, "Slack ID missing in JWT token.");
 
   try {
-    const profile = await getUserProfile(token.sub!).catch(() => null);
-    if (!profile) {
-      response.status(404).send("User not found");
-      return;
-    }
-
+    let profile = await getUserProfile(slackId).catch(() => null);
     const { method } = request;
-
     switch (method) {
       case "GET":
+        if (!profile) {
+          // The profile doesn’t exist yet, let’s create it now
+          const slackUser = await getSlackUserBySlackId(slackId);
+          profile = await createUserProfile({
+            name: slackUser.name,
+            email: slackUser.email!,
+            skills: [],
+            state: "confirmed",
+            slackUserRelationId: slackUser.id,
+          });
+        }
         response.setHeader("Content-Type", "application/json");
         response.status(200).send(JSON.stringify(profile, null, 2));
         break;
       case "PATCH":
+        if (!profile) {
+          response.status(404).send("User profile not found.");
+          return;
+        }
         // Make sure we do NOT include the `slackId` field nor `state` here
         const { name, email, skills } = request.body;
         await updateUserProfile(profile.id, { name, email, skills });
