0ef4e2026dfc957c9a5ca4abc398e8387b59f568
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-04-22 09:57:17 +0200

    Add script to migrate legacy DB tables (#569)
    * Fix bug in User Profile skill ID handling
    
    * Improve user profile creation
    
    * Add support for the legacy Volunteers table
    
    * Add script to migrate users from legacy DB tables

---
+++ lib/airtable/volunteers.ts
@@ -0,0 +1,42 @@
+import { FieldSet } from "airtable";
+import { withDefault } from "lib/decoding";
+import { unwrapRecords, volunteerManagementBase } from "./request";
+import {
+  array,
+  decodeType,
+  field,
+  record,
+  string,
+} from "typescript-json-decoder";
+
+/** The Airtable schema of the Volunteers table */
+export interface Schema extends FieldSet {
+  "Slack: E-mail": string;
+  "Slack: ID": string;
+  "Self-reported skills": ReadonlyArray<string>;
+}
+
+/** Volunteers table */
+export const volunteersTable = volunteerManagementBase<Schema>("Volunteers");
+
+/** Volunteer */
+export type Volunteer = decodeType<typeof decodeVolunteer>;
+
+/** Decode `SlackUser` from DB schema */
+export const decodeVolunteer = record({
+  email: field("Slack: E-mail", string),
+  slackId: field("Slack: ID", string),
+  skillIds: field("Self-reported skills", withDefault(array(string), [])),
+});
+
+//
+// API Calls
+//
+
+export async function getAllVolunteers(): Promise<Volunteer[]> {
+  return await volunteersTable
+    .select()
+    .all()
+    .then(unwrapRecords)
+    .then(array(decodeVolunteer));
+}
