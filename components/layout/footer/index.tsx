54d4e22a158a692b36b0b91922bb8c6f16a4d253
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-05-03 13:52:09 +0200

    Open social links in a new tab

--- components/layout/footer/index.tsx
+++ components/layout/footer/index.tsx
@@ -57,7 +57,7 @@ const Footer: React.FC = () => {
                 <S.Links>
                   {socialLinks.map(([name, url], index) => (
                     <S.LinkItem key={index}>
-                      <Link size={ButtonSize.Small} to={url}>
+                      <Link size={ButtonSize.Small} to={url} openInNewTab>
                         {name}
                       </Link>
                     </S.LinkItem>
