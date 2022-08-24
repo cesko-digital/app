e0f2cc201f2a9a0d52078ebb6fc91b3aeea071c3
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-08-24 14:06:03 +0200

    Add simplified types for Slack action blocks

--- lib/slack/interactions.ts
+++ lib/slack/interactions.ts
@@ -6,12 +6,20 @@ import {
   string,
 } from "typescript-json-decoder";
 
+/**
+ * Simplified type of a response to user interaction
+ *
+ * When user responds to a BlockKit message, Slack sends us
+ * a notification (HTTP POST) describing the interaction. In return
+ * we may send this response that will be displayed to the user again.
+ */
 export type InteractionResponse = {
   text: string;
   response_type?: "ephemeral";
   replace_original?: boolean;
 };
 
+/** A single action taken by user in a BlockKit message */
 export type BlockAction = decodeType<typeof decodeBlockAction>;
 export const decodeBlockAction = record({
   type: string,
@@ -20,6 +28,7 @@ export const decodeBlockAction = record({
   value: string,
 });
 
+/** The structure used by Slack to deliver action responses */
 export type BlockActionCallback = decodeType<typeof decodeBlockActionCallback>;
 export const decodeBlockActionCallback = record({
   type: literal("block_actions"),
