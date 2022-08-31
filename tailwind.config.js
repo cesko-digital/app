2ce1b86cba14a06dc15c38d398b6eb41a63146a7
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-08-31 17:11:50 +0200

    Move fundraising banner styles to Tailwind (#652)

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        content: "1200px",
+      },
+    },
   },
   plugins: [],
 };
