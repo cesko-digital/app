1877ff4cc65955e6f0d3b51bc6e5a8fce47df8d9
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-09-08 14:15:39 +0200

    Remove unused testing support

--- tests/e2e/english.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-
-test("should contain English text", async ({ page }) => {
-  await page.goto("/en");
-  expect(await page.title()).toBe(
-    "Česko.Digital – Skrz jedničky a nuly měníme Česko k lepšímu"
-  );
-  await expect(page.locator("h1")).toContainText("Changing Czechia");
-});
--- tests/rendering/component.tsx
+++
@@ -1,9 +0,0 @@
-interface Props {
-  name: string;
-}
-
-const TestingComponent: React.FC<Props> = ({ name }) => {
-  return <p className="nice">{name}</p>;
-};
-
-export default TestingComponent;
