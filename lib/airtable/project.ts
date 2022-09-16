5d3733e6ffb5c044ef4637b1f56ee4f3bb83efb2
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-09-16 14:05:58 +0200

    Temporarily allow projects with no coordinators

--- lib/airtable/project.ts
+++ lib/airtable/project.ts
@@ -35,7 +35,7 @@ export const decodeProject = record({
   ),
   tagIds: field("tags", withDefault(array(string), [])),
   coordinatorIds: field("coordinators", array(string)),
-  teamEngagementIds: field("team", array(string)),
+  teamEngagementIds: field("team", withDefault(array(string), [])),
   trelloUrl: optional(string),
   jiraUrl: optional(string),
   githubUrl: optional(string),
