jsfix-ci/web

Add basic page to display market-place opportunities

lib/airtable/market-place.ts
@@ -25,9 +25,12 @@ export interface Schema extends FieldSet {
 export type MarketPlaceOffer = decodeType<typeof decodeMarketPlaceOffer>;
 export const decodeMarketPlaceOffer = record({
   id: string,
+  name: string,
   owner: relationToOne,
   ownerName: relationToOne,
   ownerEmail: relationToOne,
+  ownerAvatarUrl: relationToOne,
+  ownerSlackId: relationToOne,
   text: string,
   createdAt: string,
   lastModifiedAt: string,

cesko-digital/app

lib/airtable/market-place.ts
@@ -28,6 +28,9 @@ export const decodeMarketPlaceOffer = record({
   owner: relationToZeroOrOne,
   ownerName: relationToZeroOrOne,
   ownerEmail: relationToZeroOrOne,
+  ownerAvatarUrl: relationToZeroOrOne,
+  ownerSlackId: relationToZeroOrOne,
+  name: string,
   text: string,
   createdAt: string,
   lastModifiedAt: string,