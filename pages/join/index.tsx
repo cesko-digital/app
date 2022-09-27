ad4948c28d81851720a2517df6222fa2500bd5ba
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-09-27 10:59:38 +0200

    Replace onboarding form with new title page, add basic content

---
+++ pages/join/index.tsx
@@ -0,0 +1,161 @@
+import { NextPage } from "next";
+import { Layout, Section } from "components/layout";
+
+const Page: NextPage = () => {
+  return (
+    <Layout
+      crumbs={[{ label: "Přidej se k nám!" }]}
+      head={{
+        title: "Přidej se k nám!",
+        description: `Staň se součástí Česko.Digital, největší komunity expertních dobrovolníků
+        a dobrovolnic. Rádi tě zapojíme do projektů, které tě budou bavit a kde můžeš získat
+        nové zkušenosti nebo se podělit o své nápady.`,
+      }}
+    >
+      <Section>
+        <Intro />
+        <JoinUs />
+        <HowWeWork />
+        <WhyJoin />
+        <ContactUs />
+      </Section>
+    </Layout>
+  );
+};
+
+//
+// Intro
+//
+
+const Intro = () => (
+  <section className="max-w-content m-auto mt-20 pb-10 text-lg">
+    <h1 className="text-[44px] font-bold">Měň s námi Česko k lepšímu!</h1>
+    <p>
+      Je nás už více než 5 300. Expertní dobrovolníci a zaměstnanci, členové
+      neziskovek a státní správy, partneři a nadšenci. My všichni tvoříme
+      komunitu, která pomáhá veřejné sféře s efektivním využíváním digitálních
+      technologií.
+    </p>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/gwxVBjlG8iQ"
+      title="Vítejte v Česko.Digital"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+    ></iframe>
+  </section>
+);
+
+//
+// Join Us
+//
+
+const JoinUs = () => (
+  <section className="max-w-content m-auto py-10 text-lg">
+    <h2 className="text-it">Staň se součástí komunity Česko.Digital</h2>
+    <p>
+      Projdi s námi krátkou registrací a řekni nám o sobě více. Ať už totiž
+      pomocnou ruku hledáš nebo sám nabízíš, vždy budeš{" "}
+      <b>nepostradatelným členem naší komunity</b>.
+    </p>
+    <p>
+      Věříme, že ve sdílení je síla a že{" "}
+      <b>každý z nás má ostatním co nabídnout</b>!
+    </p>
+    <a href="/join/form">Registruj se do komunity</a>
+  </section>
+);
+
+//
+// How We Work
+//
+
+const HowWeWork = () => (
+  <section className="bg-pebble">
+    <div className="max-w-content m-auto py-10 text-lg">
+      <h2>Zjisti, jak pracujeme</h2>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h3>Kde?</h3>
+          <p>
+            Online a z pohodlí svého domova, aby se mohl zapojit každý
+            odkudkoliv.
+          </p>
+        </div>
+        <div>
+          <h3>Kdy?</h3>
+          <p>
+            Ve volném čase – ráno, večer, přes víkend… Zkrátka dle našich
+            časových možností.
+          </p>
+        </div>
+        <div>
+          <h3>Jak?</h3>
+          <p>
+            Společně – v kolaborativních nástrojích, jako je Slack, Confluence,
+            Jira, Miro, GitHub nebo Figma.
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
+);
+
+//
+// Why Join?
+//
+
+const WhyJoin = () => (
+  <section className="max-w-content m-auto py-10 text-lg">
+    <h2>Proč se přidat do komunity?</h2>
+    <div className="grid grid-cols-3 gap-4">
+      <Testimonial
+        name="Petra Fritsch"
+        title="Dobrovolnice Česko.Digital"
+        blurb="V Česko.Digital cítím, že jsem skutečně součástí týmu, a neustále se učím novým věcem. Nikdy bych nevěřila, že se budu podílet na projektu, kde mohu spojit psychologii s digitalizací."
+      />
+      <Testimonial
+        name="Radek Hábl"
+        title="Zakladatel Institutu prevence a řešení předlužení"
+        blurb="Česko.Digital přináší know-how v oblasti digitálních technologií a pomáhá vytvářet smysluplné projekty. Setkávám se s lidmi, kteří mají zájem předat své zkušenosti, a sám se při spolupráci odborně posouvám v mnoha mně dosud neznámých oblastech."
+      />
+      <Testimonial
+        name="Julia Szymanska"
+        title="Employee Engagement manager, Avast Foundation"
+        blurb="Díky spolupráci s Česko.Digital můžeme pomáhat neziskovým organizacím vytvářet programy pro rozvoj, na které samy kapacitně nestačí. Expertní dobrovolnictví je také jednou ze základních hodnot společnosti Avast. Naši zaměstnanci mají tak možnost se hojně zapojovat do smysluplných projektů, které rozvíjí nejen širokou společnost, ale i je samotné."
+      />
+    </div>
+  </section>
+);
+
+const Testimonial = ({ name, title, blurb }: any) => (
+  <div>
+    <h3>{name}</h3>
+    <p>{title}</p>
+    <p>{blurb}</p>
+  </div>
+);
+
+//
+// Contact Us
+//
+
+const ContactUs = () => (
+  <section className="bg-pebble">
+    <div className="max-w-content m-auto py-10">
+      <h2>Chceš se ještě na něco doptat?</h2>
+      <p>
+        Napiš nám na{" "}
+        <a href="mailto:pomoc@cesko.digital">pomoc@cesko.digital</a>, rádi
+        pomůžeme!
+      </p>
+    </div>
+  </section>
+);
+
+//
+// Shared
+//
+
+export default Page;
