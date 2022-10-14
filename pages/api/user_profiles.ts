74e064e06e41aeb9d80b485ec7d2e9daebaf9fb7
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-10-14 14:09:03 +0200

    Add support for new “competencies” user profile field

--- pages/api/user_profiles.ts
+++ pages/api/user_profiles.ts
@@ -32,6 +32,7 @@ async function handler(request: NextApiRequest, response: NextApiResponse) {
         name,
         email,
         skills,
+        competencies: undefined,
         state: "unconfirmed",
         slackUserRelationId: undefined,
         createdAt: new Date().toISOString(),
