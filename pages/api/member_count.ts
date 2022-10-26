82b01fe34433dd2c0e66cd52b1496d949000cb13
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-10-26 16:52:54 +0200

    Improve member count API caching

--- pages/api/member_count.ts
+++ pages/api/member_count.ts
@@ -5,7 +5,10 @@ import { getMemberCount } from "lib/airtable/user-profile";
 async function handler(request: NextApiRequest, response: NextApiResponse) {
   const count = await getMemberCount();
   response.setHeader("Content-Type", "application/json");
-  response.setHeader("Cache-Control", "stale-while-revalidate=86400");
+  response.setHeader(
+    "Cache-Control",
+    "max-age=0, s-maxage=60, stale-while-revalidate=86400"
+  );
   response.status(200).send({
     timestamp: new Date(),
     count,
