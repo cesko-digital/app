f36d31ad02b53e615e964cc6dadab18c3ec70a78
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-10-27 09:27:49 +0200

    Tune member count API endpoint caching

--- pages/api/member_count.ts
+++ pages/api/member_count.ts
@@ -4,10 +4,12 @@ import { getMemberCount } from "lib/airtable/user-profile";
 
 async function handler(request: NextApiRequest, response: NextApiResponse) {
   const count = await getMemberCount();
+  const oneHour = 60 * 60;
+  const oneWeek = oneHour * 24 * 7;
   response.setHeader("Content-Type", "application/json");
   response.setHeader(
     "Cache-Control",
-    "max-age=0, s-maxage=60, stale-while-revalidate=86400"
+    `s-maxage=${oneHour}, stale-while-revalidate=${oneWeek}`
   );
   response.status(200).send({
     timestamp: new Date(),
