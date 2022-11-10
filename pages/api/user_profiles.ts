95f68bb515ccbdad024a8661a59c64c6bced4b48
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-11-10 12:28:56 +0100

    Improve request decoding in /api/user_profiles

--- pages/api/user_profiles.ts
+++ pages/api/user_profiles.ts
@@ -1,29 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { optional, record, string } from "typescript-json-decoder";
 import { addPerformanceLogging } from "lib/apm";
 import {
   createUserProfile,
   getUserProfileByMail,
 } from "lib/airtable/user-profile";
 
+const decodeRequest = record({
+  name: string,
+  email: string,
+  skills: string,
+  occupation: optional(string),
+  organizationName: optional(string),
+  profileUrl: optional(string),
+});
+
 /** Create a new, unconfirmed user profile */
 async function handler(request: NextApiRequest, response: NextApiResponse) {
-  // Validate input
-  // TBD: Update to a regular decoder
-  const { name, email, skills } = request.body;
-  const { occupation, organizationName, profileUrl } = request.body;
-  if (!name) {
-    response.status(400).send("Missing “name” param.");
-    return;
-  }
-  if (!email) {
-    response.status(400).send("Missing “email” param.");
-    return;
-  }
-  if (!skills) {
-    response.status(400).send("Missing “skills” param.");
-    return;
-  }
   try {
+    const payload = decodeRequest(request.body);
+    const { email } = payload;
     const previousProfile = await getUserProfileByMail(email).catch(() => null);
     if (previousProfile) {
       const msg = "Email already exists";
@@ -31,13 +27,9 @@ async function handler(request: NextApiRequest, response: NextApiResponse) {
       response.status(401).send(msg);
     } else {
       await createUserProfile({
-        name,
-        email,
+        ...payload,
         skills: [],
-        occupation,
-        organizationName,
-        profileUrl,
-        competencies: skills,
+        competencies: payload.skills,
         state: "unconfirmed",
         slackUserRelationId: undefined,
         createdAt: new Date().toISOString(),
