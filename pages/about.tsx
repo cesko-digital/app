e981ca967787005b015d262e57a93b64b26aa83a
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-12-06 15:52:10 +0100

    Update images & copy

--- pages/about.tsx
+++ pages/about.tsx
@@ -37,99 +37,112 @@ const IntroSection = () => (
 const CoreTeamSection = () => (
   <section className="max-w-content m-auto py-10 px-5 text-lg">
     <h2 className="mb-12 mt-0">Kmenový tým Česko.Digital</h2>
-    <p className="max-w-prose">
-      Přes 90 % rozpočtu Česko.Digital tvoří náklady na kmenový tým, který má
-      potřebné kompetence a dovednosti, udává směr organizace a pomáhá komunitě
-      dosahovat stanovených cílů.
-    </p>
     <p className="max-w-prose mb-7">
-      Jako kmenový tým zajišťujeme strategické a projektové řízení, péči o
-      komunitu, budování networku napříč organizacemi z neziskové a státní
-      správy. Věnujeme se také UX a designu služeb, včetně komunikační podpory,
-      strategie značky a PR a finančně administrativních úkonů.
+      Počet členů kmenového týmu se nepravidelně mění, ale aspirace a hodnoty
+      zůstávají stejné. Díky placeným úvazkům a potřebnému kontextu se kmenový
+      tým naplno věnuje rozvoji organizace, designu služeb a péči o komunitu.
+      Zároveň tvoří strategii organizace i značky, které pomáhají dosahovat
+      stanovených cílů. Na starosti má také komunikaci přínosu a jedinečnosti
+      organizace a budování vztahů se státem, neziskovkami, veřejností a dalšími
+      organizacemi.
     </p>
     <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7">
-      <MemberCard name="Eva Pavlíková" title="CEO" image={Eva}>
-        Vedu komunitu Česko.Digital, zajišťuju její rozvoj a směřování. Mimo
-        komunitu mě nejčastěji potkáte v roli CEO (Chief Executive Officer) nebo
-        ředitelka.{" "}
+      <MemberCard
+        name="Eva Pavlíková"
+        title="Chief Executive Officer"
+        image={Eva}
+      >
+        Vedu komunitu Česko.Digital, zajišťuji její rozvoj a směřování. Mimo
+        komunitu mě nejčastěji potkáte v roli CEO nebo ředitelka.{" "}
         <a href="https://cesko-digital.atlassian.net/wiki/spaces/CD/pages/834568254/Moje+AHA+momenty">
           Moje AHA momenty
         </a>
         .
       </MemberCard>
-      <MemberCard name="Jan Kotara" title="COO" image={Yen}>
-        Tj. provozní ředitel. Operations role umožňují ostatním se maximálně
-        soustředit na své úkoly a maximalizovat produktivitu, a to podílením se
-        na usnadnění chodu a procesů organizace.
+      <MemberCard
+        name="Jan Kotara"
+        title="Chief Operations Officer"
+        image={Yen}
+      >
+        Ve své roli umožňuji ostatním se maximálně soustředit na jejich úkoly a
+        maximalizovat produktivitu, a to podílením se na usnadnění chodu a
+        procesů organizace.
       </MemberCard>
       <MemberCard
         name="Tereza Gagnon"
-        title="Head of Partnership and Fundraising"
+        title="Head of Partnership & Fundraising"
         image={Terka}
       >
-        Zodpovídá za péči o partnery a dárce Česko.Digital tak, aby vztah
-        probíhal podle očekávání obou stran a ideálně dlouhodobě.
+        Zodpovídám za fundraising a péči o dárce a partnery Česko.Digital, aby
+        se báze našich podporovatelů rozšiřovala a pomohla tak dosahovat našich
+        cílů.
       </MemberCard>
-      <MemberCard name="Romana Pokorná" title="Project Lead" image={Romana}>
-        Pracuje na rozvoji projektové metodiky. Zajišťuje, že je transparentně a
-        srozumitelně komunikována, organizuje na daná témata setkání a
-        vzdělávání. Propojuje členy komunity s touto kompetencí.
+      <MemberCard
+        name="Romana Pokorná"
+        title="Program & Project Lead"
+        image={Romana}
+      >
+        Zodpovídám za rozvoj akcelerátoru pro neziskové organizace. Pracuji na
+        vylepšování projektové metodiky. Organizuji na daná témata setkání a
+        školení pro dobrovolníky i zástupce veřejné sféry.
       </MemberCard>
-      <MemberCard name="Anežka Müller" title="Event Lead" image={Anezka}>
-        Stará se o vše, co v Česko.Digital souvisí s oblastí eventů, ať už jsou
-        přímo pořádané Česko.Digital, participujeme na organizaci nebo se jich
-        účastníme. Zodpovídá za eventovou strategii, udržuje aktuální eventový
-        plán a má na starosti dobro-tým, který pomáhá s realizací akcí.
+      <MemberCard
+        name="Anežka Müller"
+        title="Event & Community Lead"
+        image={Anezka}
+      >
+        Starám se o vše, co v organizaci souvisí s oblastí akcí (eventů) – tedy
+        o eventovou strategii i operativu s tím spojenou. Kromě toho se také
+        věnuji péči o komunitu.
       </MemberCard>
       <MemberCard
         name="Jindřich Oukropec"
-        title="Brand Product Owner & Support"
+        title="Brand Product Owner"
         image={Jindra}
       >
-        Má na starosti nadchnout Čechy do expertního dobrovolnictví. A také
-        směřování projektu Brand Česko.Digital. Rovněž zastává roli Scrum
-        Support Mastera na projektech.
+        Mám na starosti nadchnout Čechy do expertního dobrovolnictví a také
+        směřování značky Česko.Digital. Rovněž zastávám roli Scrum Support
+        Mastera na projektech.
       </MemberCard>
       <MemberCard
         name="Terézia Palaščáková"
         title="Content Lead"
         image={Terezia}
       >
-        Má na starosti obsah a komunikaci, které propagují Česko.Digital i jeho
-        jednotlivé projekty. Zároveň vede a motivuje obsahový tým a slouží jako
-        obsahový mentor. Když je potřeba, stane se na chvíli copywriterem.
+        Mám na starosti obsah, který propaguje Česko.Digital i jeho jednotlivé
+        projekty. Zároveň vedu a motivuju dobro.obsahový tým a působím jako
+        obsahový mentor. Když je potřeba, stanu se na chvíli copywriTerkou.
       </MemberCard>
       <MemberCard
         name="Martina Habová"
-        title="Marketing and Communication Lead"
+        title="Marketing & Communication Lead"
         image={Martina}
       >
-        Zodpovídá za zviditelnění a propagaci komunity expertních dobrovolníků
-        Česko.Digital, jejich aktivit a realizovaných dopadů. Spolupracuje úzce
-        s marketingovými specialisty a dalšími kolegy.
+        Zodpovídám za zviditelnění a propagaci komunity expertních dobrovolníků,
+        jejich aktivit a dopadů. Spolupracuji s marketingovými specialisty.
       </MemberCard>
       <MemberCard
         name="Lukáš Návesník"
-        title="Digital & Production Lead"
+        title="Digital & Production Lead"
         image={Lukas}
       >
-        Má na starosti správu audiovizuálního obsahu. Vede produkční tým.
-        Zpracovává příchozí požadavky na informační grafiku na sociální sítě,
-        blogové články a vzdělávací materiály. Je spojkou mezi marketingem & IT.
+        Spravuji audiovizuální obsah a vedu dobro.produkční tým a naši
+        vzdělávací platformu edu.digital. Tvořím grafiku pro sociální sítě,
+        blogové články a vzdělávací materiály. Působím jako spojka mezi
+        marketingem & IT.
       </MemberCard>
-      <MemberCard name="Tomáš Znamenáček" title="Programátor" image={Zoul}>
-        Funguje především jako maintainer webu Česko.Digital a souvisejících
-        věcí, zejména Portálu dobrovolníka. Pomáhá dobrovolníkům, aby si taky
-        mohli hezky zaprogramovat.
+      <MemberCard name="Tomáš Znamenáček" title="Programmer" image={Zoul}>
+        Funguji především jako maintainer webu Česko.Digital a dalších interních
+        nástrojů.
       </MemberCard>
       <MemberCard
         name="Gabriela Chladilová"
-        title="NGO Program Lead & Jira Specialist"
+        title="NGO Program Lead & Jira Specialist"
         image={Gabi}
       >
-        Stará se jak o příchozí dobrovolníky, tak o jednotlivce neziskových a
-        veřejnosprávních organizací. Koordinuje příchozí projekty.
+        Starám se jak o příchozí dobrovolníky, tak o jednotlivce neziskových a
+        veřejnosprávních organizací v naší komunitě. Koordinuji také příchozí
+        projekty. Mám na starosti data o komunitě.
       </MemberCard>
     </div>
   </section>
@@ -146,9 +159,8 @@ const MemberCard = ({ name, title, image, children }: Member) => (
   <div className="border-2 border-pebble rounded-lg overflow-clip">
     <Image src={image} alt="" placeholder="blur" width="768" height="432" />
     <div className="px-4 pb-4">
-      <h3>
-        {name}, {title}
-      </h3>
+      <h3 className="mb-0">{name}</h3>
+      <h3 className="mt-0 font-normal">{title}</h3>
       {children}
     </div>
   </div>
