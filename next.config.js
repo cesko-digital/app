ee5ac03f1fd5fe506dcf82717087dd37f71bfcb3
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-02 14:48:43 +0100

    Redirect automated WordPress and *.php scans

--- next.config.js
+++ next.config.js
@@ -1,5 +1,23 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async redirects() {
+    return [
+      // Redirect automatic .php scans to keep them from spamming our logs
+      {
+        source: "/(.*).php",
+        destination:
+          "https://www.google.com/search?q=how+can+i+benefit+to+the+society",
+        permanent: false,
+      },
+      // Redirect automatic WordPress scans to keep them from spamming our logs
+      {
+        source: "/wp-(.*)",
+        destination:
+          "https://www.google.com/search?q=how+can+i+benefit+to+the+society",
+        permanent: false,
+      },
+    ];
+  },
   reactStrictMode: true,
   compiler: {
     styledComponents: true,
