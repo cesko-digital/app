2acfe6d6eaa5ec241c3f67684d19a72756538cfa
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-05 10:40:03 +0100

    Add basic overview of all, even past events

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,30 +1,60 @@
-import type { NextPage, GetStaticProps } from "next";
+import { GetStaticProps } from "next";
 import { siteData } from "lib/site-data";
-import { PortalEvent } from "lib/airtable/event";
+import {
+  compareEventsByTime,
+  isEventPast,
+  PortalEvent,
+} from "lib/airtable/event";
+import { Layout } from "components/layout";
+import EventCard from "components/dashboard/event-card";
+import { PortalProject } from "lib/airtable/project";
 
 type PageProps = {
   events: readonly PortalEvent[];
+  projects: readonly PortalProject[];
 };
 
-const Page: NextPage<PageProps> = ({ events }) => {
+const Page = ({ events, projects }: PageProps) => {
+  const futureEvents = events
+    .filter((e) => !isEventPast(e))
+    .sort(compareEventsByTime);
+  const pastEvents = events
+    .filter((e) => isEventPast(e))
+    .sort(compareEventsByTime)
+    .reverse();
+  const projectForEvent = (e: PortalEvent) =>
+    projects.find((p) => e.projectId === p.id)!;
   return (
-    <div>
-      <h1>Events</h1>
-      <p>
-        Tohle ještě není implementované. Nechtěli byste nám s tím pomoct?{" "}
-        <a href="https://github.com/cesko-digital/web/issues/356">
-          Tady je příslušný ticket
-        </a>
-        .
-      </p>
-    </div>
+    <Layout crumbs={[{ label: "Eventy" }]} head={{ title: "Eventy" }}>
+      <section className="max-w-content m-auto py-10 px-5 text-xl">
+        <h2 className="mb-12 mt-0">Nadcházející eventy</h2>
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7 mb-20">
+          {futureEvents.map((e) => (
+            <EventCard key={e.id} event={e} project={projectForEvent(e)} />
+          ))}
+        </div>
+        <h2 className="mb-12 mt-0">Starší eventy</h2>
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7">
+          {pastEvents.map((e) => (
+            <EventCard key={e.id} event={e} project={projectForEvent(e)} />
+          ))}
+        </div>
+      </section>
+    </Layout>
   );
 };
 
-export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
+export const getStaticProps: GetStaticProps<PageProps> = async () => {
+  const events = siteData.events.filter(
+    (e) => e.status === "live" || e.status === "unlisted"
+  );
+  const projects = siteData.projects.filter((p) =>
+    events.some((e) => e.projectId === p.id)
+  );
   return {
     props: {
-      events: siteData.events,
+      events,
+      projects,
     },
   };
 };
