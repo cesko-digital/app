0aabc0567f80bce473ab43ae832735953cce65b6
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-05 10:47:59 +0100

    Improve navigation and copy

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -8,6 +8,7 @@ import {
 import { Layout } from "components/layout";
 import EventCard from "components/dashboard/event-card";
 import { PortalProject } from "lib/airtable/project";
+import { Route } from "lib/utils";
 
 type PageProps = {
   events: readonly PortalEvent[];
@@ -25,15 +26,21 @@ const Page = ({ events, projects }: PageProps) => {
   const projectForEvent = (e: PortalEvent) =>
     projects.find((p) => e.projectId === p.id)!;
   return (
-    <Layout crumbs={[{ label: "Eventy" }]} head={{ title: "Eventy" }}>
+    <Layout
+      crumbs={[
+        { label: "Portál dobrovolníka", path: Route.dashboard },
+        { label: "Akce" },
+      ]}
+      head={{ title: "Akce" }}
+    >
       <section className="max-w-content m-auto py-10 px-5 text-xl">
-        <h2 className="mb-12 mt-0">Nadcházející eventy</h2>
+        <h2 className="mb-12 mt-0">Nadcházející akce</h2>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7 mb-20">
           {futureEvents.map((e) => (
             <EventCard key={e.id} event={e} project={projectForEvent(e)} />
           ))}
         </div>
-        <h2 className="mb-12 mt-0">Starší eventy</h2>
+        <h2 className="mb-12 mt-0">Starší akce</h2>
         <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-7">
           {pastEvents.map((e) => (
             <EventCard key={e.id} event={e} project={projectForEvent(e)} />
