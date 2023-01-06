5d208a1b17303c0949b964697e5a27d885ddebec
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-06 08:54:49 +0100

    Move event sorting and filtering to static phase

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,28 +1,22 @@
-import { GetStaticProps } from "next";
-import { siteData } from "lib/site-data";
+import EventCard from "components/dashboard/event-card";
+import { Layout } from "components/layout";
 import {
   compareEventsByTime,
   isEventPast,
   PortalEvent,
 } from "lib/airtable/event";
-import { Layout } from "components/layout";
-import EventCard from "components/dashboard/event-card";
 import { PortalProject } from "lib/airtable/project";
+import { siteData } from "lib/site-data";
 import { Route } from "lib/utils";
+import { GetStaticProps } from "next";
 
 type PageProps = {
-  events: readonly PortalEvent[];
-  projects: readonly PortalProject[];
+  futureEvents: PortalEvent[];
+  pastEvents: PortalEvent[];
+  projects: PortalProject[];
 };
 
-const Page = ({ events, projects }: PageProps) => {
-  const futureEvents = events
-    .filter((e) => !isEventPast(e))
-    .sort(compareEventsByTime);
-  const pastEvents = events
-    .filter((e) => isEventPast(e))
-    .sort(compareEventsByTime)
-    .reverse();
+const Page = ({ futureEvents, pastEvents, projects }: PageProps) => {
   const projectForEvent = (e: PortalEvent) =>
     projects.find((p) => e.projectId === p.id)!;
   return (
@@ -55,14 +49,24 @@ export const getStaticProps: GetStaticProps<PageProps> = async () => {
   const events = siteData.events.filter(
     (e) => e.status === "live" || e.status === "unlisted"
   );
+  const futureEvents = events
+    .filter((e) => !isEventPast(e))
+    .sort(compareEventsByTime);
+  const pastEvents = events
+    .filter((e) => isEventPast(e))
+    .sort(compareEventsByTime)
+    .reverse();
   const projects = siteData.projects.filter((p) =>
     events.some((e) => e.projectId === p.id)
   );
   return {
     props: {
-      events,
+      futureEvents,
+      pastEvents,
       projects,
     },
+    // Regenerate every five minutes to refresh event info
+    revalidate: 60 * 5,
   };
 };
 
