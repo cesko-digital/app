439eabb91cdc4d19b728bd68909d60070954c784
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-10 14:05:20 +0100

    Skip users too old to qualify for welcome messages

--- bin/send-welcome-messages.ts
+++ bin/send-welcome-messages.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S npx ts-node -r tsconfig-paths/register -r dotenv-flow/config
 
-import { getAllUserProfiles } from "lib/airtable/user-profile";
+import { getAllUserProfiles, UserProfile } from "lib/airtable/user-profile";
 import { join, resolve } from "path";
 import { readdirSync, readFileSync } from "fs";
 import { sendWelcomeMessage } from "lib/onboarding";
@@ -18,7 +18,7 @@ async function sendWelcomeMessages() {
   const allUsers = (await getAllUserProfiles())
     // Only message users that have already joined Slack
     .filter((user) => user.state === "confirmed")
-    // And make double sure we have those Slack IDs to message
+    // Make double sure we have those Slack IDs to message
     .filter((user) => !!user.slackId);
   for (const [day, message] of parseWelcomeMessages()) {
     if (day === 0) {
@@ -28,7 +28,7 @@ async function sendWelcomeMessages() {
     // Filter out users that have just joined `day` days ago
     const recipients = allUsers.filter((u) => u.daysSinceRegistered === day);
     console.log(
-      `Found ${recipients.length} users that have joined ${day} days ago.`
+      `Found ${recipients.length} users that have joined exactly ${day} days ago.`
     );
     // Sanity check
     if (recipients.length > maxUserCount) {
@@ -39,11 +39,33 @@ async function sendWelcomeMessages() {
     }
     // Send messages
     for (const recipient of recipients) {
-      console.debug(
-        `Sending message for day #${day} to user ${recipient.slackId}.`
-      );
-      //sendWelcomeMessage(recipient.slackId!, message);
+      if (qualifiesForWelcomeMessages(recipient)) {
+        console.debug(
+          `Sending message for day #${day} to user ${recipient.slackId}.`
+        );
+        //sendWelcomeMessage(recipient.slackId!, message);
+      } else {
+        console.debug(
+          `Skipping ${recipient.slackId}, user too old to qualify (joined at ${recipient.createdAt}).`
+        );
+      }
     }
+  }
+}
+
+/**
+ * Is user profile new enough to receive the welcome messages?
+ *
+ * We want to make sure everyone either receives the full welcome sequence
+ * or no messages at all.
+ */
+function qualifiesForWelcomeMessages(user: UserProfile): boolean {
+  if (user.createdAt) {
+    const creationDate = new Date(user.createdAt);
+    const featureLaunchDate = new Date(2023, 0, 10);
+    return creationDate > featureLaunchDate;
+  } else {
+    return false;
   }
 }
 
