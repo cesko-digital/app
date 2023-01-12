306a193b8f5de28116c966d5e445bdb4e62294e2
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-12 14:11:54 +0100

    Add bin/run.ts script to simplify hacking

---
+++ bin/run.ts
@@ -0,0 +1,11 @@
+#!/usr/bin/env -S npx ts-node -r tsconfig-paths/register -r dotenv-flow/config
+
+const toJSON = (val: any) => JSON.stringify(val, null, 2);
+
+async function main() {
+  // Your code here
+}
+
+main().catch((error) => console.log(error));
+
+export {};
