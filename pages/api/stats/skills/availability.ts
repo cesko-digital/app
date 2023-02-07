df85ffa3f29e8b8d6d3f3dedc864c27239db2c9c
Tomáš Znamenáček <zoul@zoul.cz>
2023-02-07 10:22:02 +0100

    Add skill stats endpoint

---
+++ pages/api/stats/skills/availability.ts
@@ -0,0 +1,24 @@
+import { getAllUserProfiles } from "lib/airtable/user-profile";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(request: NextApiRequest, response: NextApiResponse) {
+  const userProfiles = await getAllUserProfiles();
+  const nonEmptySkills = userProfiles
+    .filter((userProfile) => userProfile.state === "confirmed")
+    .map((userProfile) => userProfile.skills)
+    .filter((skill) => skill !== "");
+  response.setHeader("Access-Control-Allow-Origin", "*");
+  response.setHeader("Content-Type", "text/csv");
+  response.setHeader(
+    "Cache-Control",
+    "max-age=0, s-maxage=3600, stale-while-revalidate"
+  );
+  response.status(200);
+  response.write(`Has skills, ${nonEmptySkills.length}\n`);
+  response.write(
+    `Has no skills, ${userProfiles.length - nonEmptySkills.length}\n`
+  );
+  response.end();
+}
+
+export default handler;
