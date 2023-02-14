c4accd82ff45a8bc699c6216e4e5d051ed801aa5
Anezka Muller <anezka.muller@gmail.com>
2023-02-14 13:19:25 +0100

    Add redirect for Meetup #3

--- next.config.js
+++ next.config.js
@@ -107,6 +107,11 @@ module.exports = withAxiom({
       // Older URLs we would like to support as a courtesy
       //
       {
+        source: "/events/meetup-ceskodigital-1",
+        destination: "/events/meetup-ceskodigital-3",
+        permanent: true,
+      },  
+      {
         source: "/events/hack-day-1",
         destination: "/events/hack-day-2",
         permanent: true,
