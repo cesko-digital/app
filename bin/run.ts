3fac470857c304a2be95ee141e55a80f4f751840
Tomáš Znamenáček <zoul@zoul.cz>
2023-03-08 10:00:55 +0100

    Clean sample shell script

--- bin/run.ts
+++ bin/run.ts
@@ -1,16 +1,9 @@
 #!/usr/bin/env -S npx ts-node -r tsconfig-paths/register -r dotenv-flow/config
 
-import { readFileSync } from "fs";
-import { getAllOpportunities } from "lib/airtable/opportunity";
-import { getPageBreakdown } from "lib/data-sources/plausible";
-
 const toJSON = (val: any) => JSON.stringify(val, null, 2);
 
 async function main() {
-  const response = await getPageBreakdown();
-  console.log(
-    toJSON(response.filter((row) => row.page.startsWith("/opportunities/")))
-  );
+  // Your code here
 }
 
 main().catch((error) => console.log(error));
