f787f98e95064a80f13fd4ea55adae8437b688a9
jindrich-oukropec <69157075+jindrich-oukropec@users.noreply.github.com>
2023-03-09 10:24:02 +0100

    Update about.tsx

--- pages/about.tsx
+++ pages/about.tsx
@@ -205,9 +205,7 @@ const CoreTeamSection = () => (
         title="Brand Product Owner"
         image={Jindra}
       >
-        Mám na starosti nadchnout Čechy do expertního dobrovolnictví a také
-        směřování značky Česko.Digital. Rovněž zastávám roli Scrum Support
-        Mastera na projektech.
+        Mám na starosti značku, reputaci a komunikační strategii organizace. Zabývám se tím, jak ke komu komunikujeme, jaká očekávání vzbuzujeme s ohledem na organizační strategii. Rovněž zastávám roli Scrum Support Mastera na projektech.
       </PersonCard>
       <PersonCard
         name="Terézia Palaščáková"
