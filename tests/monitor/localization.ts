9a077ffd3e730dfbd2d4bf88e914db2d85d1b88d
Tomáš Znamenáček <zoul@zoul.cz>
2023-03-15 09:52:15 +0100

    Add bare-bones localization test

---
+++ tests/monitor/localization.ts
@@ -0,0 +1,12 @@
+import { test, expect } from "@playwright/test";
+
+/**
+ * Test if the localized website looks right
+ *
+ * The localized website is kind of hard to test, because it’s
+ * localized automatically by Weglot from the production site.
+ */
+test("Localization looks right", async ({ page }) => {
+  await page.goto("https://en.cesko.digital");
+  await expect(page.locator("h1")).toHaveText(/Changing Czechia/);
+});
