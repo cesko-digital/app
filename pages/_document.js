0edfd3ffd967702edd6199bf479e75f0911c1b37
Tomáš Znamenáček <zoul@zoul.cz>
2023-03-16 14:11:00 +0100

    Move Weglot from _document to layout, include on English version only

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
-import Document, { Html, Main, Head, NextScript } from "next/document";
+import Document from "next/document";
 import { ServerStyleSheet } from "styled-components";
-import Script from "next/script";
 
 // This was introduced to make Styled Components work. If we later
 // switch to a different styling method, we can safely delete the whole
@@ -30,30 +29,5 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
-  }
-
-  // We customize the render method to get Weglot translation up working
-  // event before the document gets interactive. See Next.js documentation:
-  //
-  // https://nextjs.org/docs/messages/no-before-interactive-script-outside-document
-  //
-  // TBD: Could we skip the script completely when serving the Czech version?
-  render() {
-    return (
-      <Html>
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-          <Script
-            type="text/javascript"
-            src="https://cdn.weglot.com/weglot.min.js"
-          />
-          <Script id="weglot">
-            {`Weglot.initialize({ api_key: '${process.env.NEXT_PUBLIC_WEGLOT_API_KEY}', hide_switcher: true });`}
-          </Script>
-        </body>
-      </Html>
-    );
   }
 }
