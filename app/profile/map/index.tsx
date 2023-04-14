090ac251652fa8436ac73e5e861bce8349a57244
Tomáš Znamenáček <zoul@zoul.cz>
2023-04-14 11:33:10 +0200

    Add district management UI to user profile

--- app/profile/map/index.tsx
+++ app/profile/map/index.tsx
@@ -1,5 +1,46 @@
+import { useEffect, useState } from "react";
+import { useUserProfile } from "app/profile/hooks";
+
 export const VolunteerMapPrefs = () => {
-  return <Section>Here be dragons!</Section>;
+  const [districts, setDistricts] = useState("");
+  const { profile, updateProfile, isUpdating } = useUserProfile();
+
+  const save = () => {
+    updateProfile({ availableInDistricts: districts });
+  };
+
+  useEffect(() => {
+    setDistricts(profile?.availableInDistricts ?? "");
+  }, [profile]);
+
+  return (
+    <Section>
+      <label htmlFor="districts" className="block mb-2">
+        Ve kterých okresech ČR se vyskytuješ?
+      </label>
+      <div className="flex flex-row gap-2">
+        <input
+          id="districts"
+          type="text"
+          value={districts}
+          disabled={isUpdating}
+          className="rounded-md border-2 border-gray p-2 w-full"
+          placeholder={profile ? "Brno, Praha, …" : ""}
+          onChange={(e) => setDistricts(e.target.value)}
+        ></input>
+        {isUpdating && (
+          <button className="btn-disabled flex-none" disabled={true}>
+            Moment…
+          </button>
+        )}
+        {!isUpdating && (
+          <button className="btn-primary flex-none" onClick={save}>
+            Uložit
+          </button>
+        )}
+      </div>
+    </Section>
+  );
 };
 
 const Section = ({ children }: { children: React.ReactNode }) => (
