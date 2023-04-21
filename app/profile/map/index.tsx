7c1dfaa13550514969defa708abbf5f71389496c
Tomáš Znamenáček <zoul@zoul.cz>
2023-04-21 10:31:19 +0200

    Show actual district stats on map

--- app/profile/map/index.tsx
+++ app/profile/map/index.tsx
@@ -2,21 +2,15 @@ import { useEffect, useState } from "react";
 import { useUserProfile } from "app/profile/hooks";
 import dynamic from "next/dynamic";
 
+type DistrictStats = Record<string, number>;
+
 // The component has to be imported dynamically since Leaflet doesn’t
 // play nice with server-side rendering.
 const Map = dynamic(() => import("./map"), { ssr: false });
 
 export const VolunteerMapPrefs = () => {
-  return (
-    <div className="text-lg max-w-prose grid grid-cols gap-7 mb-10">
-      <InputControls />
-      <MapSection />
-    </div>
-  );
-};
-
-const InputControls = () => {
   const [districts, setDistricts] = useState("");
+  const [stats, setStats] = useState<DistrictStats>({});
   const { profile, updateProfile, isUpdating } = useUserProfile();
 
   // Update profile when the Save button is clicked
@@ -29,38 +23,49 @@ const InputControls = () => {
     setDistricts(profile?.availableInDistricts ?? "");
   }, [profile]);
 
+  // Reload map data when profile finishes updating
+  useEffect(() => {
+    const loadData = async () => {
+      await fetch("/stats/districts?format=json")
+        .then((response) => response.json())
+        .then(setStats);
+    };
+    if (!isUpdating) {
+      loadData();
+    }
+  }, [isUpdating]);
+
   return (
-    <section>
-      <label htmlFor="districts" className="block mb-2">
-        Ve kterých okresech ČR se vyskytuješ?
-      </label>
-      <div className="flex flex-row gap-2">
-        <input
-          id="districts"
-          type="text"
-          value={districts}
-          disabled={isUpdating}
-          className="rounded-md border-2 border-gray p-2 w-full"
-          placeholder={profile ? "Brno, Praha, …" : ""}
-          onChange={(e) => setDistricts(e.target.value)}
-        ></input>
-        {isUpdating && (
-          <button className="btn-disabled flex-none" disabled={true}>
-            Moment…
-          </button>
-        )}
-        {!isUpdating && (
-          <button className="btn-primary flex-none" onClick={save}>
-            Uložit
-          </button>
-        )}
-      </div>
-    </section>
+    <div className="text-lg max-w-prose grid grid-cols gap-7 mb-10">
+      <section>
+        <label htmlFor="districts" className="block mb-2">
+          Ve kterých okresech ČR se vyskytuješ?
+        </label>
+        <div className="flex flex-row gap-2">
+          <input
+            id="districts"
+            type="text"
+            value={districts}
+            disabled={isUpdating}
+            className="rounded-md border-2 border-gray p-2 w-full"
+            placeholder={profile ? "Brno, Praha, …" : ""}
+            onChange={(e) => setDistricts(e.target.value)}
+          ></input>
+          {isUpdating && (
+            <button className="btn-disabled flex-none" disabled={true}>
+              Moment…
+            </button>
+          )}
+          {!isUpdating && (
+            <button className="btn-primary flex-none" onClick={save}>
+              Uložit
+            </button>
+          )}
+        </div>
+      </section>
+      <section>
+        <Map style={{ height: "400px" }} stats={stats} />
+      </section>
+    </div>
   );
 };
-
-const MapSection = () => (
-  <section>
-    <Map center={[49.8, 15.5]} zoom={7} style={{ height: "400px" }} />
-  </section>
-);
--- app/profile/map/map.tsx
+++ app/profile/map/map.tsx
@@ -1,32 +1,43 @@
-import { districts } from "./districts";
-import {
-  MapContainer,
-  MapContainerProps,
-  Marker,
-  Popup,
-  TileLayer,
-} from "react-leaflet";
+import { districts as knownDistrictsWithCoords } from "./districts";
+import { CSSProperties } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
-const Map = (props: MapContainerProps) => (
-  <div>
-    <link
-      rel="stylesheet"
-      href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-      integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-      crossOrigin=""
-    />
-    <MapContainer {...props}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+type DistrictMapProps = {
+  style?: CSSProperties;
+  stats: Record<string, number>;
+};
+
+const DistrictMap = ({ style, stats }: DistrictMapProps) => {
+  const pins = Object.entries(stats)
+    .filter(([name]) => !!knownDistrictsWithCoords[name])
+    .map(([name, count]) => ({
+      name,
+      count,
+      coords: knownDistrictsWithCoords[name],
+    }));
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+        integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+        crossOrigin=""
       />
-      {Object.entries(districts).map(([name, coords]) => (
-        <Marker key={name} position={coords as any}>
-          <Popup>okres {name}</Popup>
-        </Marker>
-      ))}
-    </MapContainer>
-  </div>
-);
+      <MapContainer center={[49.8, 15.5]} zoom={7} style={style}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {pins.map(({ name, count, coords }) => (
+          <Marker key={name} position={coords as any}>
+            <Popup>
+              okres {name}: {count}
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
 
-export default Map;
+export default DistrictMap;
