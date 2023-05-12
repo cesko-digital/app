0e877bb6066a82d781f7018b78888a431d2bc6b4
Tomáš Znamenáček <zoul@zoul.cz>
2023-05-12 15:32:26 +0200

    Add experimental custom iCal event feed

---
+++ app/events/feed2.ical/route.ts
@@ -0,0 +1,23 @@
+import ical from "ical-generator";
+import { getAllEvents } from "lib/airtable/event";
+import { Route, absolute } from "lib/routing";
+import { markdownToHTML } from "lib/utils";
+
+export async function GET(): Promise<Response> {
+  const events = await getAllEvents("Live Events");
+  const calendar = ical({ name: "Akce Česko.Digital" });
+  for (const event of events) {
+    calendar.createEvent({
+      summary: event.name,
+      description: markdownToHTML(event.description.source),
+      location: event.locationTitle,
+      url: absolute(Route.toEvent(event)),
+      start: new Date(event.startTime),
+      end: event.endTime ? new Date(event.endTime) : undefined,
+    });
+  }
+  return new Response(calendar.toString(), {
+    status: 200,
+    headers: { "Content-Type": "text/plain" },
+  });
+}
