6c17e6407128fbdec58fa7a260913b75cbd1fe2e
Tomáš Znamenáček <zoul@zoul.cz>
2023-05-25 09:50:45 +0200

    Update homepage counters

--- components/home/numbers/index.tsx
+++ components/home/numbers/index.tsx
@@ -1,5 +1,4 @@
 import * as S from "./styles";
-import strings from "content/strings.json";
 import { useContext } from "react";
 import { SectionContent } from "components/layout";
 import { ThemeContext } from "styled-components";
@@ -10,26 +9,25 @@ interface Props {
 
 const Numbers = ({ memberCount }: Props) => {
   const theme = useContext(ThemeContext);
-  const numbers = strings.pages.homepage.sections.numbers;
   return (
     <S.CustomSection backgroundColor={theme.colors.pebble}>
       <SectionContent verticalPadding={70}>
         <S.Wrapper>
           <S.Item key="first">
             <S.Value>{Math.floor(memberCount / 100) * 100}+</S.Value>
-            <S.Subtitle>{numbers.first.subtitle}</S.Subtitle>
+            <S.Subtitle>členů komunity</S.Subtitle>
           </S.Item>
           <S.Item key="second">
-            <S.Value>{numbers.second.value}</S.Value>
-            <S.Subtitle>{numbers.second.subtitle}</S.Subtitle>
+            <S.Value>16+</S.Value>
+            <S.Subtitle>rozjetých projektů</S.Subtitle>
           </S.Item>
           <S.Item key="third">
-            <S.Value>{numbers.third.value}</S.Value>
-            <S.Subtitle>{numbers.third.subtitle}</S.Subtitle>
+            <S.Value>28</S.Value>
+            <S.Subtitle>akcelerovaných projektů</S.Subtitle>
           </S.Item>
           <S.Item key="fourth">
-            <S.Value>{numbers.fourth.value}</S.Value>
-            <S.Subtitle>{numbers.fourth.subtitle}</S.Subtitle>
+            <S.Value>4 000 000+</S.Value>
+            <S.Subtitle>lidí využilo naše projekty</S.Subtitle>
           </S.Item>
         </S.Wrapper>
       </SectionContent>
