7cef285550638eb8aa40742da025953a69a0357e
Tomáš Znamenáček <zoul@zoul.cz>
2023-05-25 11:51:21 +0200

    Generate anchors for headings in project description

--- app/projects/schema.ts
+++ app/projects/schema.ts
@@ -1,4 +1,10 @@
-import { Schema, Config } from "@markdoc/markdoc";
+import {
+  Schema,
+  Config,
+  Tag,
+  Node,
+  RenderableTreeNode,
+} from "@markdoc/markdoc";
 
 /**
  * A callout is a “box that stands out”, a part of a document we want to emphasize
@@ -7,6 +13,25 @@ import { Schema, Config } from "@markdoc/markdoc";
  */
 export const callout: Schema = {
   render: "Callout",
+};
+
+/** Custom heading node that auto-generates heading IDs */
+export const heading: Schema = {
+  children: ["inline"],
+  attributes: {
+    id: { type: String },
+    level: { type: Number, required: true, default: 1 },
+  },
+  transform(node: Node, config: Config) {
+    const attributes = node.transformAttributes(config);
+    const children = node.transformChildren(config);
+    const id = generateID(children, attributes);
+    return new Tag(
+      `h${node.attributes["level"]}`,
+      { ...attributes, id },
+      children
+    );
+  },
 };
 
 /**
@@ -19,4 +44,36 @@ export const projectDescriptionConfig: Config = {
   tags: {
     callout,
   },
+  nodes: {
+    heading,
+  },
 };
+
+//
+// Helpers
+//
+
+function generateID(
+  children: RenderableTreeNode[],
+  attributes: Record<string, any>
+) {
+  const stringify = (node: RenderableTreeNode): string => {
+    if (Tag.isTag(node)) {
+      return node.children.map(stringify).join(" ");
+    } else {
+      return `${node}`;
+    }
+  };
+  if (attributes.id && typeof attributes.id === "string") {
+    return attributes.id;
+  }
+  const id = children.map(stringify).join(" ");
+  return slugify(id);
+}
+
+export const slugify = (s: string) =>
+  s
+    .toLocaleLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .replace(/[^a-z0-9]+/g, "-");
