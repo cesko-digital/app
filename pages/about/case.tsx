031c3dfe6d4bac6b279b660ae0c08feb24493dd4
Tomáš Znamenáček <zoul@zoul.cz>
2023-06-16 14:46:40 +0200

    Add comment about app vs. page router

--- pages/about/case.tsx
+++ pages/about/case.tsx
@@ -1,6 +1,13 @@
 import { Layout } from "components/layout";
 import CasePage from "app/about/case/_page";
 
+/**
+ * The “Case for Support” page
+ *
+ * We try to keep most of the new code in the `app` router, but still use the `page` router
+ * for compatibility reasons – so the entry point is here, but all of the code and assets are
+ * stored in the `app` folder.
+ */
 const Page = () => (
   <Layout crumbs={[{ label: "O nás" }]} head={{ title: "Case for Support" }}>
     <CasePage />
