af325f281162407c2940a968a2738d32186a6fab
Tomáš Znamenáček <zoul@zoul.cz>
2023-09-08 11:34:22 +0200

    Add systemic change banner to Parters page

--- lib/utils.ts
+++ lib/utils.ts
@@ -128,3 +128,12 @@ export function hashDigest(
   shasum.update([...params, secret].join(":"));
   return shasum.digest("hex").slice(0, 10);
 }
+
+/** Filler text to aid with developing */
+export const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+aliquip ex ea commodo consequat. Duis aute irure dolor in
+reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+culpa qui officia deserunt mollit anim id est laborum.`;
