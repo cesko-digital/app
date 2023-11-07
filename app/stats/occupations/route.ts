de117c29c6b22b7feed0c5b3d39934c275f1ee49
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-07 12:39:31 +0100

    Add first batch of statistic API endpoints

---
+++ app/stats/occupations/route.ts
@@ -0,0 +1,41 @@
+import { getAllUserProfiles } from "src/data/user-profile";
+import { notEmpty, unique } from "src/utils";
+
+export async function GET() {
+  const userProfiles = await getAllUserProfiles(
+    "Profiles with Occupation Data"
+  );
+  const allOccupations = userProfiles
+    // Extract user’s occupation
+    .map((userProfile) => userProfile.occupation)
+    // Make sure the occupation is not null
+    .filter(notEmpty);
+
+  const uniqueOccupations = unique(allOccupations);
+
+  const labels = {
+    "freelancing": "na volné noze",
+    "state": "státní sektor",
+    "studying": "studující",
+    "parental-leave": "na rodičovské dovolené",
+    "private-sector": "soukromý sektor",
+    "non-profit": "neziskový sektor",
+    "other": "jiné",
+    "looking-for-job": "hledám práci",
+  } as Record<string, string>;
+
+  let response = "";
+  for (const occupation of uniqueOccupations) {
+    response += `${labels[occupation!]}, ${
+      allOccupations.filter((o) => o === occupation).length
+    }\n`;
+  }
+
+  return new Response(response, {
+    status: 200,
+    headers: {
+      "Content-Type": "text/csv; charset=utf-8",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
+}
---
+++ app/stats/smoke.e2e.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+/** Endpoints to test */
+const endpoints = [
+  "/stats/districts",
+  "/stats/occupations",
+  "/stats/opportunities/sources",
+  "/stats/opportunities/visits",
+  "/stats/skills/availability",
+  "/stats/skills/categories",
+  "/stats/skills/subcategories",
+  "/stats/trends/new-comers?year=2022&fill=true",
+  "/stats/trends/new-comers-engagements?year=2022&fill=true",
+  "/stats/trends/new-engagements?year=2022&fill=true",
+];
+
+for (const endpoint of endpoints) {
+  test(`Stats endpoint works: ${endpoint}`, async ({ page }) => {
+    const response = await page.request.get(endpoint);
+    expect(response).toBeOK();
+    expect(response.headers()["content-type"]).toEqual(
+      "text/csv; charset=utf-8"
+    );
+  });
+}
