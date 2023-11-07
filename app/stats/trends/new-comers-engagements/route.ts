8dab599c174af62489737fc0d9abffac9122935d
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-07 15:06:38 +0100

    Add remaining statistics API endpoints

---
+++ app/stats/trends/new-comers-engagements/route.ts
@@ -0,0 +1,21 @@
+import { getCsvResponse } from "../trend-response";
+import { buildTrendStats } from "../trend-stats";
+import { buildTrendOptions } from "../trend-request";
+import { generateNewComersTrend, generateNewEngagementsTrend } from "../trends";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  return await getCsvResponse(async function (): Promise<string | null> {
+    return await buildTrendStats(buildTrendOptions(searchParams), [
+      {
+        title: "Zapojeni do projektu",
+        generate: generateNewEngagementsTrend,
+      },
+      {
+        title: "Noví dobrovolníci",
+        generate: generateNewComersTrend,
+      },
+    ]);
+  });
+}
---
+++ app/stats/trends/new-engagements/route.ts
@@ -0,0 +1,15 @@
+import { getCsvResponse } from "../trend-response";
+import { buildTrendStats } from "../trend-stats";
+import { buildTrendOptions } from "../trend-request";
+import { generateNewEngagementsTrend } from "../trends";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+
+  return await getCsvResponse(async function (): Promise<string | null> {
+    return await buildTrendStats(
+      buildTrendOptions(searchParams),
+      generateNewEngagementsTrend
+    );
+  });
+}
