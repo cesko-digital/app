c5137951c76bea20b7361a742d5c35fc1d596e83
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-07 12:23:55 +0100

    Add project decoding

--- src/data/event.test.ts
+++ src/data/event.test.ts
@@ -1,4 +1,4 @@
-import { decodeEvent, getEventDuration } from "./events";
+import { decodeEvent, getEventDuration } from "./event";
 
 test("Decode event", () => {
   expect(
---
+++ src/data/project.ts
@@ -0,0 +1,72 @@
+import { unwrapRecords, webBase } from "./airtable";
+import {
+  decodeJSONString,
+  decodeValidItemsFromArray,
+  markdown,
+  optionalArray,
+  withDefault,
+} from "src/decoding";
+import {
+  array,
+  boolean,
+  decodeType,
+  field,
+  optional,
+  record,
+  string,
+  union,
+} from "typescript-json-decoder";
+
+//
+// Decoding
+//
+
+export type Project = decodeType<typeof decodeProject>;
+export const decodeProject = record({
+  id: field("ID", string),
+  name: string,
+  slug: string,
+  tagline: string,
+  description: markdown,
+  coverImageUrl: string,
+  logoUrl: withDefault(
+    string,
+    "https://data.cesko.digital/web/projects/generic-logo.png"
+  ),
+  highlighted: withDefault(boolean, false),
+  state: withDefault(
+    union("draft", "running", "finished", "incubating", "internal"),
+    "draft"
+  ),
+  tagIds: field("tags", optionalArray(string)),
+  teamEngagementIds: field("team", optionalArray(string)),
+  youTubePlaylistId: optional(string),
+  links: field(
+    "serializedLinks",
+    decodeJSONString(
+      array(
+        record({
+          name: string,
+          url: string,
+          featured: boolean,
+        })
+      )
+    )
+  ),
+});
+
+//
+// API Calls
+//
+
+/** Projects table */
+export const projectsTable = webBase("Projects");
+
+/** Get all projects */
+export async function getAllProjects(): Promise<Project[]> {
+  return await projectsTable
+    .select()
+    .all()
+    .then(unwrapRecords)
+    .then(decodeValidItemsFromArray(decodeProject, "Projects"));
+}
