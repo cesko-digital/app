6b4e5199bc0f31c1c01840c63974910a3a030dbf
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-08 10:37:56 +0100

    Add default Tailwind theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,16 +2,21 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      maxWidth: {
+        content: "1200px",
+      },
+      colors: {
+        pebble: "#F9F9F9",
+        it: "#0000FF",
+        violet: "CCCCFF",
+        yellow: "#FFF6A3",
+        plum: "#080831",
+        gray: "#EDEDEF",
       },
     },
   },
