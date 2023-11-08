40a8056ddec140dc71eab647ea76031da0c6de5e
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-08 11:16:13 +0100

    Extract project card to a shared component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import { ProjectCard } from "components/ProjectCard";
 import {
   Event,
   compareEventsByTime,
@@ -58,35 +58,6 @@ export default async function Home() {
 //
 // Projects
 //
-
-const ProjectCard = (project: Project) => (
-  <a
-    className="block border-2 border-gray rounded-xl overflow-clip relative"
-    key={project.id}
-    href={Route.toProject(project)}
-  >
-    <div className="aspect-[2] relative">
-      <Image
-        src={project.coverImageUrl}
-        alt=""
-        className="bg-gray"
-        objectFit="cover"
-        fill
-      />
-    </div>
-    <Image
-      src={project.logoUrl}
-      alt=""
-      width={80}
-      height={80}
-      className="drop-shadow-xl rounded-full -mt-[40px] ml-10 bg-white"
-    />
-    <div className="p-7 flex flex-col gap-4">
-      <h3 className="typo-title3">{project.name}</h3>
-      <p>{project.tagline}</p>
-    </div>
-  </a>
-);
 
 async function getFeaturedProjects(): Promise<Project[]> {
   const canBeFeatured = (p: Project) =>
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import { Project } from "src/data/project";
+import { Route } from "src/routing";
+
+export const ProjectCard = (project: Project) => (
+  <a
+    className="block border-2 border-gray rounded-xl overflow-clip relative hover:border-it hover:shadow-lg"
+    key={project.id}
+    href={Route.toProject(project)}
+  >
+    <div className="aspect-[2] relative">
+      <Image
+        src={project.coverImageUrl}
+        alt=""
+        className="bg-gray"
+        objectFit="cover"
+        fill
+      />
+    </div>
+    <Image
+      src={project.logoUrl}
+      alt=""
+      width={80}
+      height={80}
+      className="drop-shadow-xl rounded-full -mt-[40px] ml-10 bg-white"
+    />
+    <div className="p-7 flex flex-col gap-4">
+      <h3 className="typo-title3">{project.name}</h3>
+      <p>{project.tagline}</p>
+    </div>
+  </a>
+);
