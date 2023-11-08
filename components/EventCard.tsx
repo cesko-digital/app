0e928f383380943b5b9d36d79b49998d33aad48a
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-08 11:25:39 +0100

    Add event cards

---
+++ components/EventCard.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import { Event } from "src/data/event";
+import { Route } from "src/routing";
+
+export const EventCard = (event: Event) => (
+  <a
+    className="block overflow-clip rounded-xl border-2 border-gray hover:border-it hover:shadow-lg"
+    key={event.id}
+    href={Route.toEvent(event)}
+  >
+    <div className="aspect-[2] relative">
+      {/* TBD: Make the cover non-optional */}
+      <Image
+        src={event.coverImageUrl!}
+        alt=""
+        className="bg-gray"
+        objectFit="cover"
+        fill
+      />
+    </div>
+    <div className="p-7 flex flex-col gap-4">
+      <h3 className="typo-title3">{event.name}</h3>
+      <p>{event.summary}</p>
+    </div>
+  </a>
+);
