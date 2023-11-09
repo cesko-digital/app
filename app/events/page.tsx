ff82c031f1262e48d22dcd9fbb59339a01f8085d
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-09 11:02:34 +0100

    Badge events with project logos

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,10 +1,17 @@
 import { Breadcrumbs } from "components/Breadcrumbs";
 import { EventCard } from "components/EventCard";
-import { compareEventsByTime, getAllEvents, isEventPast } from "src/data/event";
+import {
+  Event,
+  compareEventsByTime,
+  getAllEvents,
+  isEventPast,
+} from "src/data/event";
+import { getAllProjects } from "src/data/project";
 import { loremIpsum } from "src/utils";
 
 async function Page() {
   const events = await getAllEvents("Live Events");
+  const projects = await getAllProjects();
   const futureEvents = events
     .filter((e) => !isEventPast(e))
     .sort(compareEventsByTime);
@@ -12,6 +19,8 @@ async function Page() {
     .filter((e) => isEventPast(e))
     .sort(compareEventsByTime)
     .reverse();
+  const projectForEvent = (e: Event) =>
+    projects.find((p) => p.id === e.projectId);
 
   return (
     <main className="py-20 px-7 max-w-content m-auto">
@@ -26,14 +35,23 @@ async function Page() {
       <h2 className="typo-title2 mb-4">Nadcházející akce</h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7 mb-20">
         {futureEvents.map((e) => (
-          <EventCard key={e.id} event={e} />
+          <EventCard
+            badgeImageUrl={projectForEvent(e)?.logoUrl}
+            key={e.id}
+            event={e}
+          />
         ))}
       </div>
 
       <h2 className="typo-title2 mb-4">Proběhlé akce</h2>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7 mb-20">
         {pastEvents.map((e) => (
-          <EventCard key={e.id} event={e} fade={true} />
+          <EventCard
+            badgeImageUrl={projectForEvent(e)?.logoUrl}
+            key={e.id}
+            event={e}
+            fade={true}
+          />
         ))}
       </div>
     </main>
