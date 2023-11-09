4bd1dc997210c376ed23bafca3eaeede70ec092a
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-09 15:41:12 +0100

    Show project links on project pages

---
+++ components/icons/index.ts
@@ -0,0 +1,33 @@
+import { default as Slack } from "./slack";
+import { default as Trello } from "./trello";
+import { default as Jira } from "./jira";
+import { default as GitHub } from "./github";
+import { default as GenericWebsite } from "./website";
+import { default as Airtable } from "./airtable";
+import { default as Asana } from "./asana";
+import { default as Confluence } from "./confluence";
+import { default as Figma } from "./figma";
+import { default as GoogleCalendar } from "./gcalendar";
+import { default as GoogleDocs } from "./gdocs";
+import { default as GoogleDrive } from "./gdrive";
+import { default as Miro } from "./miro";
+import { default as YouTube } from "./youtube";
+
+const Icons = {
+  Slack,
+  Trello,
+  Jira,
+  GitHub,
+  GenericWebsite,
+  Airtable,
+  Asana,
+  Confluence,
+  Figma,
+  GoogleCalendar,
+  GoogleDocs,
+  GoogleDrive,
+  Miro,
+  YouTube,
+};
+
+export default Icons;
