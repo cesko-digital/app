87bdb84ec1af82085a4dbc2c776810254842b9ab
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-13 10:38:25 +0100

    Improve opportunity selection for home page

--- src/data/queries.ts
+++ src/data/queries.ts
@@ -1,9 +1,11 @@
+import { getRandomElem, shuffleInPlace, unique } from "src/utils";
 import {
   Event,
   compareEventsByTime,
   findEventsForProject,
   isEventPast,
 } from "./event";
+import { Opportunity, getAllOpportunities } from "./opportunity";
 import { Project } from "./project";
 
 /**
@@ -24,4 +26,30 @@ export async function getFeaturedEventsForProject(
     ...futureEvents.sort(compareEventsByTime),
     ...pastEvents.sort(compareEventsByTime).reverse(),
   ].slice(0, count);
+}
+
+/**
+ * Get featured opportunities
+ *
+ * - We want to see opportunities from different projects if possible
+ * - Don’t show opportunities from projects that are not running
+ */
+export async function getFeaturedOpportunities(
+  count = 3
+): Promise<Opportunity[]> {
+  const opportunities = await getAllOpportunities("Show to Users");
+  // Let’s pick `count` projects to feature
+  const featuredProjectIds = shuffleInPlace(
+    unique(opportunities.map((o) => o.projectId))
+  ).slice(0, count);
+  if (featuredProjectIds.length < count) {
+    // If we don’t have that many, just return random `count` opportunities
+    return shuffleInPlace(opportunities.slice(0, count));
+  } else {
+    // Otherwise pick a random opportunity from each featured project
+    return featuredProjectIds
+      .map((id) => opportunities.filter((o) => o.projectId === id))
+      .map((ops) => getRandomElem(ops))
+      .flat();
+  }
 }
