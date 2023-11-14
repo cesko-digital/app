d2e556c3c7a6ca96448e7c8164812ee6a207ba57
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-14 15:19:58 +0100

    Extract generic Card component; add related blog posts to project page

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -1,17 +1,14 @@
-import clsx from "clsx";
-import Image from "next/image";
-import Link from "next/link";
 import { Event, isEventPast } from "src/data/event";
 import { Route } from "src/routing";
 import { TextPill } from "./TextPill";
+import { Card } from "./Card";
 
 export type Props = {
   event: Event;
   badgeImageUrl?: string;
-  fade?: boolean;
 };
 
-export const EventCard = ({ event, badgeImageUrl, fade = false }: Props) => {
+export const EventCard = ({ event, badgeImageUrl }: Props) => {
   const past = isEventPast(event);
   const pastDateStyle: Intl.DateTimeFormatOptions = {
     day: "numeric",
@@ -29,34 +26,14 @@ export const EventCard = ({ event, badgeImageUrl, fade = false }: Props) => {
     "cs-CZ",
     past ? pastDateStyle : futureDateStyle
   );
+  /* TBD: Make the cover non-optional? */
   return (
-    <Link
-      className="block overflow-clip rounded-xl border-2 border-gray hover:border-it hover:shadow-lg"
-      key={event.id}
-      href={Route.toEvent(event)}
+    <Card
+      coverImageUrl={event.coverImageUrl!}
+      badgeImageUrl={badgeImageUrl}
+      linkUrl={Route.toEvent(event)}
+      fade={past}
     >
-      <div className="aspect-[2] relative">
-        {/* TBD: Make the cover non-optional? */}
-        <Image
-          src={event.coverImageUrl!}
-          sizes="(min-width: 1200px) 400px, 100vw"
-          alt=""
-          className={clsx(
-            "bg-gray object-cover",
-            fade && "grayscale-[75%] opacity-75"
-          )}
-          fill
-        />
-      </div>
-      {badgeImageUrl && (
-        <Image
-          src={badgeImageUrl}
-          className="-mt-[55px] ml-7 rounded-full bg-white drop-shadow-xl"
-          alt=""
-          width={80}
-          height={80}
-        />
-      )}
       <div className="-mt-2 p-7 flex flex-col gap-3">
         <div className="typo-caption">
           {isEventPast(event) && <TextPill text="proběhlo" inverted />}
@@ -68,6 +45,6 @@ export const EventCard = ({ event, badgeImageUrl, fade = false }: Props) => {
         <h3 className="typo-title3">{event.name}</h3>
         <p>{event.summary}</p>
       </div>
-    </Link>
+    </Card>
   );
 };
