6d3cc36843b179fb3569eb5fbb6e6c1c2146aa27
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-14 09:53:25 +0100

    Add simple persistent cache

---
+++ src/cache.ts
@@ -0,0 +1,24 @@
+import { Cache } from "file-system-cache";
+import { hashDigest } from "./utils";
+
+const sharedCache = new Cache({ ttl: 60 * 5 });
+
+type Producer<T> = (...args: any[]) => Promise<T>;
+
+export function memoize<ReturnType, F extends Producer<ReturnType>>(
+  originalFunction: F,
+  key: string = originalFunction.name
+): F {
+  const memoized = async (...args: Parameters<F>): Promise<ReturnType> => {
+    const compoundKey = hashDigest([key, ...args]);
+    const cachedValue = await sharedCache.get(compoundKey);
+    if (cachedValue) {
+      return cachedValue;
+    } else {
+      const freshValue = await originalFunction(...args);
+      sharedCache.set(compoundKey, freshValue);
+      return freshValue;
+    }
+  };
+  return memoized as F;
+}
