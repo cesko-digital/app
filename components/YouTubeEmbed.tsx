ab568d7c0a4bc71c516c20e62322dec42591e6ca
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-14 15:52:15 +0100

    Simplify & document react-lite-youtube-embed re-export

--- components/YouTubeEmbed.tsx
+++ components/YouTubeEmbed.tsx
@@ -1,3 +1,6 @@
 "use client";
-import LiteYouTubeEmbed from "react-lite-youtube-embed";
-export default LiteYouTubeEmbed;
+
+// This is just re-exporting the component with the client
+// marker above. Once `react-lite-youtube-embed` adds `"use client"`
+// to their codebase, we may drop this.
+export { default } from "react-lite-youtube-embed";
