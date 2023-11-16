79cd23c4e07b0d12825efcbdb3ace4441c12d82b
Tomáš Znamenáček <zoul@zoul.cz>
2023-11-16 14:57:07 +0100

    Add /profile/me API endpoint

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth";
 import SlackProvider from "next-auth/providers/slack";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
 const handler = NextAuth({
   providers: [
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     SlackProvider({
-      clientId: process.env.SLACK_CLIENT_ID as string,
-      clientSecret: process.env.SLACK_CLIENT_SECRET as string,
+      clientId: process.env.SLACK_CLIENT_ID!,
+      clientSecret: process.env.SLACK_CLIENT_SECRET!,
       authorization: {
         params: {
           team: "TG21XF887",
---
+++ app/profile/me/route.ts
@@ -0,0 +1,111 @@
+import { NextResponse, type NextRequest } from "next/server";
+
+import { getToken } from "next-auth/jwt";
+import { optional, record, string } from "typescript-json-decoder";
+
+import { getSlackUserBySlackId } from "~/src/data/slack-user";
+import {
+  createUserProfile,
+  getUserProfile,
+  getUserProfileByMail,
+  updateUserProfile,
+} from "~/src/data/user-profile";
+import { normalizeEmailAddress } from "~/src/utils";
+
+/** Create new user profile (called by the onboarding form) */
+export async function POST(request: NextRequest): Promise<Response> {
+  const decodeRequest = record({
+    name: string,
+    email: string,
+    skills: string,
+    gdprPolicyAcceptedAt: string,
+    codeOfConductAcceptedAt: string,
+    occupation: optional(string),
+    availableInDistricts: optional(string),
+    organizationName: optional(string),
+    profileUrl: optional(string),
+  });
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    const payload = decodeRequest(await request.json());
+    const email = normalizeEmailAddress(payload.email);
+    const previousProfile = await getUserProfileByMail(email).catch(() => null);
+    if (previousProfile) {
+      const msg = "Email already exists";
+      console.error(msg);
+      return new Response(msg, { status: 401 });
+    } else {
+      await createUserProfile({
+        ...payload,
+        email,
+        state: "unconfirmed",
+        slackUserRelationId: undefined,
+        createdAt: new Date().toISOString(),
+      });
+      return new Response("User profile created.", { status: 201 });
+    }
+  } catch (e) {
+    console.error(e);
+    return new Response("Sorry :(", { status: 500 });
+  }
+}
+
+/** Get user profile, used for stuff like displaying user preferences */
+export async function GET(request: NextRequest) {
+  return withAuthenticatedUser(request, async (slackId) => {
+    let profile = await getUserProfile(slackId).catch(() => null);
+    if (!profile) {
+      // The profile doesn’t exist yet, let’s create it now
+      const slackUser = await getSlackUserBySlackId(slackId);
+      profile = await createUserProfile({
+        name: slackUser.name,
+        email: slackUser.email!,
+        skills: "",
+        occupation: undefined,
+        organizationName: undefined,
+        profileUrl: undefined,
+        availableInDistricts: "",
+        state: "confirmed",
+        slackUserRelationId: slackUser.id,
+        createdAt: new Date().toISOString(),
+        gdprPolicyAcceptedAt: undefined,
+        codeOfConductAcceptedAt: undefined,
+      });
+    }
+    return NextResponse.json(profile);
+  });
+}
+
+/** Change user profile, used for stuff like updating user preferences */
+export async function PATCH(request: NextRequest) {
+  return withAuthenticatedUser(request, async (slackId) => {
+    const profile = await getUserProfile(slackId).catch(() => null);
+    if (!profile) {
+      return new Response("User profile not found.", { status: 404 });
+    }
+    // Make sure we do NOT include the `slackId` field nor `state` here
+    /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+    const { name, skills, notificationFlags, availableInDistricts } =
+      await request.json();
+    await updateUserProfile(profile.id, {
+      name,
+      skills,
+      notificationFlags,
+      availableInDistricts,
+    });
+    /* eslint-enable @typescript-eslint/no-unsafe-assignment */
+    return new Response("Updated", { status: 200 });
+  });
+}
+
+async function withAuthenticatedUser(
+  request: NextRequest,
+  action: (slackId: string) => Promise<Response>,
+): Promise<Response> {
+  const token = await getToken({ req: request });
+  if (!token?.sub) {
+    return new Response("Authentication required", { status: 401 });
+  } else {
+    return await action(token.sub);
+  }
+}
