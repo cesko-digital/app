8215ef1fbad95573666bc73dc748f2c2273cb384
Tomáš Znamenáček <zoul@zoul.cz>
2023-12-08 11:36:18 +0100

    Improve 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,36 @@
+export default async function Page() {
+  return (
+    <main className="m-auto flex max-w-content flex-col gap-20 px-7 py-20">
+      <section>
+        <h1 className="typo-title mb-4 mt-20 text-center">
+          404: Stránka nenalezena
+        </h1>
+        <p className="m-auto max-w-prose">
+          Nedávno jsme web hodně měnili, možná nám něco uteklo? Pardon! Pokud
+          jste v našem Slacku, ozvěte se nám prosím v kanálu{" "}
+          <a
+            href="https://cesko-digital.slack.com/archives/CHG9NA23D"
+            className="typo-link"
+          >
+            #ceskodigital-web
+          </a>
+          , případně můžete napsat na{" "}
+          <a
+            href="mailto:zoul@cesko.digital?Subject=Chybějící stránka na webu"
+            className="typo-link"
+          >
+            zoul@cesko.digital
+          </a>{" "}
+          nebo rovnou{" "}
+          <a
+            href="https://github.com/cesko-digital/web/issues/new"
+            className="typo-link"
+          >
+            vytvořit nový ticket na GitHubu
+          </a>
+          . Děkujeme!
+        </p>
+      </section>
+    </main>
+  );
+}
