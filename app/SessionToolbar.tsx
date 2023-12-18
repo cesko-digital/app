84876c9efd10b490cf087963ed1ffa2243d3bb15
Tomáš Znamenáček <zoul@zoul.cz>
2023-12-18 16:36:35 +0100

    Improve session toolbar

--- app/SessionToolbar.tsx
+++ app/SessionToolbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Fragment } from "react";
+import Image from "next/image";
 import Link from "next/link";
 
 import { type Session } from "next-auth";
@@ -10,28 +11,48 @@ import { Route } from "~/src/routing";
 
 export const SessionToolbar = ({ session }: { session: Session | null }) => {
   return (
-    <div className="ml-auto flex flex-row gap-4">
-      {session && (
-        <Fragment>
-          <span>{session.user?.email}</span>
-          <a
-            className="typo-link cursor-pointer"
-            onClick={() => signOut({ callbackUrl: "/" })}
-          >
-            Odhlásit
-          </a>
-        </Fragment>
-      )}
-      {!session && (
-        <Fragment>
-          <Link className="typo-link" href={Route.signIn}>
-            Přihlásit se
-          </Link>
-          <Link className="typo-link" href={Route.register}>
-            Registrovat
-          </Link>
-        </Fragment>
-      )}
+    <div className="ml-auto flex flex-row items-center gap-4">
+      {session?.user && <SignedInButtons session={session} />}
+      {!session && <SignedOutButtons />}
     </div>
   );
 };
+
+const SignedInButtons = ({ session }: { session: Session }) => {
+  const avatarImage =
+    session.user?.image ??
+    "https://data.cesko.digital/people/generic-profile.jpg";
+  const avatarTitle =
+    session.user?.name && session.user?.email
+      ? `${session.user?.name} (${session.user?.email})`
+      : undefined;
+  return (
+    <Fragment>
+      <a
+        className="typo-link cursor-pointer"
+        onClick={() => signOut({ callbackUrl: "/" })}
+      >
+        Odhlásit
+      </a>
+      <Image
+        className="rounded-full bg-gray shadow"
+        src={avatarImage}
+        alt={session.user?.name ?? "Uživatelský profil"}
+        title={avatarTitle}
+        width={40}
+        height={40}
+      />
+    </Fragment>
+  );
+};
+
+const SignedOutButtons = () => (
+  <Fragment>
+    <Link className="typo-link" href={Route.signIn}>
+      Přihlásit se
+    </Link>
+    <Link className="typo-link" href={Route.register}>
+      Registrovat
+    </Link>
+  </Fragment>
+);
