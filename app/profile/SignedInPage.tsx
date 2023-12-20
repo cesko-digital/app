712a56d2bf5ca06e719398683792a77c61baad02
Tomáš Znamenáček <zoul@zoul.cz>
2023-12-20 13:49:43 +0100

    Add basic tab bar to user profile page

--- app/profile/SignedInPage.tsx
+++ app/profile/SignedInPage.tsx
@@ -2,20 +2,19 @@
 
 import { type Session } from "next-auth";
 
-import { TabBar } from "~/components/TabBar";
+import { SimpleTabBar } from "~/components/TabBar";
 
 export const SignedInPage = ({ session }: { session: Session }) => {
+  const tabs = [
+    { title: "Dovednosti", content: <section>TBD: Dovednosti</section> },
+    { title: "Newslettery", content: <section>TBD: Newslettery</section> },
+    { title: "Upozornění", content: <section>TBD: Upozornění</section> },
+    { title: "Mapa komunity", content: <section>TBD: Mapa komunity</section> },
+  ];
   return (
     <section>
       <h1 className="typo-title mb-4 mt-7">{session?.user?.name}</h1>
-      <TabBar
-        items={[
-          { key: "skills", title: "Dovednosti" },
-          { key: "newsletters", title: "Newslettery" },
-          { key: "notifications", title: "Upozornění" },
-          { key: "map", title: "Mapa komunity" },
-        ]}
-      />
+      <SimpleTabBar items={tabs} />
     </section>
   );
 };
--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -2,39 +2,43 @@ import { useState } from "react";
 
 import clsx from "clsx";
 
-export type Item = {
-  key: string;
+export type Item<Key> = {
+  key: Key;
   title: string;
 };
 
-export type Props = {
-  items: Item[];
-  defaultActiveKey?: string;
-  onChange?: (key: string) => void;
+export type Props<Key> = {
+  items: Item<Key>[];
+  defaultActiveKey?: Key;
+  onChange?: (key: Key) => void;
 };
 
 /** TBD: Improve on mobile */
-export const TabBar = ({ items, defaultActiveKey, onChange }: Props) => {
-  const [activeKey, setActiveKey] = useState<string>(
+export const TabBar = <Key extends string>({
+  items,
+  defaultActiveKey,
+  onChange,
+}: Props<Key>) => {
+  const [activeKey, setActiveKey] = useState<Key>(
     defaultActiveKey ?? items[0].key,
   );
 
-  const clickHandler = (key: string) => {
+  const clickHandler = (key: Key) => {
     setActiveKey(key);
     if (onChange) {
       onChange(key);
     }
   };
 
-  const Tab = (item: Item) => {
+  const Tab = (item: Item<Key>) => {
     const isActive = activeKey === item.key;
     return (
       <li key={item.key} className="me-2">
         <a
           onClick={() => clickHandler(item.key)}
           className={clsx(
-            "hover:text-gray-600 inline-block cursor-pointer rounded-t-lg border-b-2 border-transparent p-4",
-            isActive && "border-it",
+            "inline-block cursor-pointer border-b-2 p-4",
+            isActive ? "border-it" : "border-transparent",
           )}
         >
           {item.title}
@@ -46,6 +50,24 @@ export const TabBar = ({ items, defaultActiveKey, onChange }: Props) => {
   return (
     <div className="border-b border-gravel text-center">
       <ul className="-mb-px flex flex-wrap">{items.map(Tab)}</ul>
+    </div>
+  );
+};
+
+export type SimpleItem = {
+  title: string;
+  content: React.ReactNode;
+};
+
+export const SimpleTabBar = ({ items }: { items: SimpleItem[] }) => {
+  const [activeKey, setActiveKey] = useState(items[0].title);
+  return (
+    <div className="flex flex-col gap-7">
+      <TabBar
+        items={items.map(({ title }) => ({ key: title, title }))}
+        onChange={setActiveKey}
+      />
+      {items.find((item) => item.title === activeKey)?.content}
     </div>
   );
 };
