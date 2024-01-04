c007b4bb3c4682350e1a93effc92e7710ba45bda
Tomáš Znamenáček <zoul@zoul.cz>
2024-01-04 14:15:41 +0100

    Fix route type

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 
 import { authOptions } from "~/src/auth";
 
-const handler: unknown = NextAuth(authOptions);
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
