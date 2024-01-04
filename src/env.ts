226fd3c0f34d4e9e7e3cd1157295448ecfa3c0f6
martinwenisch <martin@muhu.digital>
2024-01-04 10:57:27 +0100

    VERCEL_URL

--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,3 @@
 export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL
   ? process.env.NEXT_PUBLIC_SITE_URL
-  : `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  : `https://${process.env.VERCEL_URL}`;
