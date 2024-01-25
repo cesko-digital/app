9a200e8051a53f24487908f3c0a34f2e40865ca2
Tomáš Znamenáček <zoul@zoul.cz>
2024-01-25 08:27:58 +0100

    Always display last chart version from Datawrapper

--- app/stats/DatawrapperChart.tsx
+++ app/stats/DatawrapperChart.tsx
@@ -4,21 +4,20 @@ import { useEffect, useRef } from "react";
 
 type Props = {
   id: string;
-  version?: number;
   title?: string;
 };
 
-export const DatawrapperChart = ({ id, version = 1 }: Props) => {
+export const DatawrapperChart = ({ id }: Props) => {
   const elem = useRef<HTMLDivElement | null>(null);
   const script = useRef<HTMLScriptElement>();
   useEffect(() => {
     if (!script.current) {
       script.current = document.createElement("script");
-      script.current.src = `https://datawrapper.dwcdn.net/${id}/embed.js?v=${version}`;
+      script.current.src = `https://datawrapper.dwcdn.net/${id}/embed.js`;
       script.current.defer = true;
       elem.current!.appendChild(script.current);
     }
-  }, [elem, script, id, version]);
+  }, [elem, script, id]);
 
   return (
     <div ref={elem} style={{ marginTop: "30px", marginBottom: "30px" }}></div>
