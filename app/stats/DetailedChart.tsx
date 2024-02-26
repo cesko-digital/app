4b5c57349e9ecbf9adeda5502020fe8c71a31f0f
Tomáš Znamenáček <zoul@zoul.cz>
2024-02-26 12:34:23 +0100

    Add year to detailed metric chart

--- app/stats/DetailedChart.tsx
+++ app/stats/DetailedChart.tsx
@@ -16,6 +16,7 @@ export const DetailedChart = ({
   const dateFormatter = new Intl.DateTimeFormat("cs-CZ", {
     day: "numeric",
     month: "long",
+    year: "2-digit",
   });
   const numberFormatter = new Intl.NumberFormat("cs-CZ", {
     notation: "compact",
