283a86b072bb75328ef85f3916dfb7c9f8a02966
Tomáš Znamenáček <zoul@zoul.cz>
2024-03-06 10:53:14 +0100

    Improve 404 event logging

--- components/Mark404.tsx
+++ components/Mark404.tsx
@@ -7,9 +7,7 @@ import { trackCustomEvent } from "~/src/plausible/events";
 /** Client-side component that logs a 404 event for current page into our Plausible Analytics */
 export const Mark404 = () => {
   useEffect(() => {
-    document.addEventListener("DOMContentLoaded", () => {
-      trackCustomEvent("404", { props: { path: document.location.pathname } });
-    });
+    trackCustomEvent("404", { props: { path: document.location.pathname } });
   }, []);
   return null;
 };
