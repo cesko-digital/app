b31c509b54bdbe2982f8cec69d50393b961f32d2
Tomáš Znamenáček <zoul@zoul.cz>
2024-03-19 09:24:49 +0100

    Add a dummy /api/deploy webhook for Slack slash command

---
+++ app/api/deploy/route.ts
@@ -0,0 +1,6 @@
+export async function POST(): Promise<Response> {
+  return new Response(
+    "Ruční přenasazování už není potřeba, viz tady: https://cesko-digital.slack.com/archives/CHG9NA23D/p1710836236418969",
+    { status: 200 },
+  );
+}
