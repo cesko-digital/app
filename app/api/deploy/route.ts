852029966948e01ee97e7622eba173374c675bf9
Tomáš Znamenáček <zoul@zoul.cz>
2024-03-21 15:53:55 +0100

    Add endpoint documentation for /api/*

--- app/api/deploy/route.ts
+++ app/api/deploy/route.ts
@@ -1,3 +1,8 @@
+/**
+ * In older versions we had a `deploy` Slack slash command to redeploy the website.
+ * This is now not needed, but as a courtesy to users we provide this simple feedback
+ * if someone still tries to use the command.
+ */
 export async function POST(): Promise<Response> {
   return new Response(
     "Ruční přenasazování už není potřeba, viz tady: https://cesko-digital.slack.com/archives/CHG9NA23D/p1710836236418969",
