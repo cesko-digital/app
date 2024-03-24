8eb690e094e1f4cbd1548a86759f6fbb26bd43ae
Jakub Drahoš <jakubdrahosJD@seznam.cz>
2024-03-24 18:00:59 +0100

    refactor: make script template more explicit

--- bin/run.template.ts
+++ bin/run.template.ts
@@ -1,5 +1,10 @@
 #!/usr/bin/env -S npx ts-node -r tsconfig-paths/register -r dotenv-flow/config
 
+/**
+ * This is a template file for any TS scripts that you want to run directly from the command line.
+ * You should use it as a starting point for your own scripts; just don't forget to delete this comment. (:
+ */
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
 const toJSON = (val: any) => JSON.stringify(val, null, 2);
 
