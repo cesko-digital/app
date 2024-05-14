7dd115c8399df6da36064f26746f2c3eda760eb0
laura.baluchova <laura.menova@gmail.com>
2024-05-14 12:33:35 +0200

    use NextApiResponse and NextApiRequest

--- app/stats/report/route.ts
+++ app/stats/report/route.ts
@@ -1,41 +1,51 @@
-import { type NextRequest } from "next/server";
-import { sendDirectMessage } from "~/src/slack/message";
-import { getMetricsNewsletterHeader, getMetricsNewsletterDetail } from "~/src/slack/metricsNewsletter";
-import { getMetricsForPreviousMonth } from "~/src/data/metrics";
+// app/stats/report/route.ts
 
-/**
- * This endpoint is called when our users type the `/metriky` slash command
- *
- * https://api.slack.com/interactivity/slash-commands
- */
+import { type NextApiRequest, type NextApiResponse } from "next";
+
+import { getMetricsForPreviousMonth } from "~/src/data/metrics";
+import { sendDirectMessage } from "~/src/slack/message";
+import {
+  getMetricsNewsletterDetail,
+  getMetricsNewsletterHeader,
+} from "~/src/slack/metricsNewsletter";
 
 const { SLACK_METRICS_TOKEN = "" } = process.env;
 
-export async function POST(request: NextRequest): Promise<Response> {
-  const formData = await request.formData();
-  const user_id = formData.get("user_id");  
-  const metrics = await getMetricsForPreviousMonth();
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === "POST") {
+    try {
+      const { user_id } = req.body;
 
-  if (user_id && typeof user_id !== "string") {
-    return new Response(null, { status: 400 });
-  }
- 
-  if (user_id) {   
-    const result = await sendDirectMessage({
+      if (!user_id || typeof user_id !== "string") {
+        return res.status(400).json({ error: "Invalid user ID provided" });
+      }
+
+      const metrics = await getMetricsForPreviousMonth();
+
+      const result = await sendDirectMessage({
         token: SLACK_METRICS_TOKEN,
         user: user_id,
         text: "Nové metriky za poslední měsíc jsou právě tady! 📊🚀",
         blocks: getMetricsNewsletterHeader(metrics),
-         });
-        await sendDirectMessage({
+      });
+
+      await sendDirectMessage({
         token: SLACK_METRICS_TOKEN,
         user: user_id,
         text: "Detail metrik za poslední měsíc",
         blocks: getMetricsNewsletterDetail(metrics),
         thread_ts: result.ts,
-         })}
-  
+      });
 
-  return new Response(null, { status: 204 });
+      return res.status(204).end();
+    } catch (error) {
+      console.error("Error processing request:", error);
+      return res.status(500).json({ error: "Internal Server Error" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 }
-
