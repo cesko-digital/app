f70cb01e28212007eecf489806cb667a54cd18d1
Tomáš Znamenáček <zoul@zoul.cz>
2024-05-15 11:31:44 +0200

    Refactor code

--- app/MobileNav.tsx
+++ app/MobileNav.tsx
@@ -24,25 +24,29 @@ export const MobileNav = () => {
 
   const CrossIcon = () => <div className="pb-2 text-[70px] text-it">✕</div>;
 
-  const ToggleMenu = () => (
+  const MenuToggle = () => (
     <button
       onClick={(_) => setOpen((open) => !open)}
       className="cursor-pointer"
       aria-label="Menu"
       aria-expanded={isOpen}
     >
-      {!isOpen && <Ham />}
-      {isOpen && <CrossIcon />}
+      {isOpen ? <CrossIcon /> : <Ham />}
     </button>
   );
+
   return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between md:hidden">
         <SquareLogo />
-        <ToggleMenu />
+        <MenuToggle />
       </div>
       <div
         className={clsx(
+          // We could hide the menu simply by not including the appropriate
+          // nodes when the menu is closed, but that keeps resetting the menu
+          // state such as session management items, so we use zero height instead
+          // to preserve the state. This also allows us to animate the transition.
           "flex flex-col gap-7 overflow-clip transition-all",
           isOpen ? "mt-10" : "h-0",
         )}
