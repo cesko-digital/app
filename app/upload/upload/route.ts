4d4e633213a22dc575c0ffc6540486c27931954d
Tomáš Znamenáček <zoul@zoul.cz>
2024-05-29 10:38:38 +0200

    Only allow asset upload for authorized users

--- app/upload/upload/route.ts
+++ app/upload/upload/route.ts
@@ -1,8 +1,22 @@
 import { NextResponse } from "next/server";
 
 import { put } from "@vercel/blob";
+import { getServerSession } from "next-auth";
+
+import { authOptions, type OurUser } from "~/src/auth";
+import { getUserProfile } from "~/src/data/user-profile";
 
 export async function POST(request: Request): Promise<Response> {
+  const session = await getServerSession(authOptions);
+  if (!session?.user) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
+  const user = await getUserProfile((session.user as OurUser).id);
+  if (!user?.featureFlags.includes("assetUpload")) {
+    return new Response("Unauthorized", { status: 401 });
+  }
+
   const { searchParams } = new URL(request.url);
 
   const filename = searchParams.get("filename");
