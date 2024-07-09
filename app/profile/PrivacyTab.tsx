9992c2268ac1ffa32bc5ef8a7061c17e10c9cd72
laura.baluchova <laura.menova@gmail.com>
2024-07-09 18:54:47 +0200

    add checkbox to enable public profile, adjust texts

--- app/profile/PrivacyTab.tsx
+++ app/profile/PrivacyTab.tsx
@@ -7,8 +7,37 @@ export const PrivacyTab = () => {
     url: "/profile/me",
     writeKeys: ["privacyFlags"],
   });
+
   return (
     <div>
+      <p className="mb-4">
+        Může nějakou dobu trvat, než se změny těchto nastavení projeví.
+      </p>
+      <label className="flex items-center">
+        <input
+          checked={model?.privacyFlags.includes("enablePublicProfile")}
+          type="checkbox"
+          className="mr-3"
+          disabled={updating}
+          onChange={(e) => {
+            setModel({
+              ...model!,
+              privacyFlags: setFlag(
+                model!.privacyFlags,
+                "enablePublicProfile",
+                e.target.checked,
+              ),
+            });
+            console.log(model?.privacyFlags);
+          }}
+        ></input>
+        Chci mít veřejný profil
+      </label>
+      <div className="typo-caption mb-4 ml-6">
+        Budete uvedený v seznamu uživatelů a kdokoliv si bude moct prohlédnou
+        údaje jako třeba vaše projekty nebo kontakt na vás
+      </div>
+
       <label className="flex items-center">
         <input
           checked={model?.privacyFlags.includes("hidePublicTeamMembership")}
@@ -28,9 +57,9 @@ export const PrivacyTab = () => {
         ></input>
         Nechci veřejně ukazovat svoje zapojení na projektech
       </label>
-      <p className="typo-caption ml-6 mt-1">
-        Může nějakou dobu trvat, než se změna nastavení projeví.
-      </p>
+      <div className="typo-caption mb-4 ml-6">
+        Nebudete uvedený v detailu projektu jako součást týmu
+      </div>
     </div>
   );
 };
