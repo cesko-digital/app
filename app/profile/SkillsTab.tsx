993d4615198e50182143aa0296572d6eb3f8d2f9
laura.baluchova <laura.menova@gmail.com>
2024-07-12 20:55:05 +0200

    add text field with bio, add possibility to change bio

--- app/profile/SkillsTab.tsx
+++ app/profile/SkillsTab.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { usePatchedJSONResource } from "~/components/hooks/resource";
 import { SkillPicker } from "~/components/SkillPicker";
 import { type UserProfile } from "~/src/data/user-profile";
@@ -11,7 +13,14 @@ import skills from "~/src/skills/skills.json";
 export const SkillsTab = () => {
   const { model, updating, setModel } = usePatchedJSONResource<UserProfile>({
     url: "/profile/me",
-    writeKeys: ["skills"],
+    writeKeys: ["skills", "bio"],
+  });
+
+  const defaultBio = model?.bio ?? "";
+
+  const [actualBio, setActualBio] = useState({
+    bio: defaultBio,
+    submissionState: "no_changes",
   });
 
   const selection = model ? decodeSkillSelection(model.skills) : {};
@@ -21,8 +30,52 @@ export const SkillsTab = () => {
     setModel({ ...model!, skills: encodeSkillSelection(selection) });
   };
 
+  const enableSubmitButton =
+    actualBio.submissionState !== "no_changes" &&
+    actualBio.submissionState !== "submitted_successfully";
+
+  const submitButtonLabel =
+    actualBio.submissionState === "no_changes"
+      ? "Uložit změny"
+      : actualBio.submissionState === "submitted_successfully"
+        ? "Úspěšně uloženo 🎉"
+        : "Uložit změny";
+
   return (
     <section className="mb-10">
+      <p className="mb-4">Řekni něco málo o sobě, ať tě lidé lépe poznají</p>
+      <textarea
+        className="block w-full rounded-md border-2 border-gray p-2"
+        defaultValue={actualBio.bio}
+        onChange={(e) =>
+          setActualBio(() => ({
+            submissionState: "",
+            bio: e.target.value,
+          }))
+        }
+        placeholder="Čemu se věnuješ? Co tě baví? Do jakých projektů tě láká se zapojit?"
+      />
+
+      <button
+        onClick={() => {
+          setModel({
+            ...model!,
+            bio: actualBio.bio,
+          }),
+            setActualBio((prevState) => ({
+              ...prevState,
+              submissionState: "submitted_successfully",
+            }));
+        }}
+        className={
+          enableSubmitButton
+            ? "btn-primary mb-10 mt-2 block"
+            : "btn-disabled mb-10 mt-2 block"
+        }
+      >
+        {submitButtonLabel}
+      </button>
+
       <p className="mb-4">
         Co chceš v Česko.Digital dělat? Dej nám to vědět, ať ti můžeme různými
         kanály nabízet relevantnější příležitosti.
