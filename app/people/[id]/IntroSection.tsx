bb1f0caba5c18bc0689db10e3b9e533606ea255f
Tomáš Znamenáček <zoul@zoul.cz>
2024-07-18 09:16:09 +0200

    Add professional profile link to user profile page

--- app/people/[id]/IntroSection.tsx
+++ app/people/[id]/IntroSection.tsx
@@ -8,18 +8,31 @@ import { Route } from "~/src/routing";
 
 export const IntroSection = ({ profile }: { profile: UserProfile }) => {
   const isCurrentUser = useCurrentUser(profile.id);
+  const tidyUrl = (s: string) =>
+    s.replace(/^https?:\/\//, "").replace(/\/$/, "");
   return (
-    <section className="flex flex-col gap-4">
-      <div className="flex flex-col justify-between gap-y-2 md:flex-row">
-        <h1 className="typo-title mb-2">{profile.name}</h1>
-        {isCurrentUser && (
-          <Link href={Route.account} className="btn-inverted">
-            Upravit
-          </Link>
+    <section>
+      <div>
+        <div className="flex flex-col justify-between gap-y-2 max-md:mb-3 md:flex-row">
+          <h1 className="typo-title mb-2">{profile.name}</h1>
+          {isCurrentUser && (
+            <Link href={Route.account} className="btn-inverted">
+              Upravit
+            </Link>
+          )}
+        </div>
+      </div>
+      <div className="text-gravel">
+        <p className="mb-1">{getUserHashtags(profile).join(" ")}</p>
+        {profile.profileUrl && (
+          <p>
+            <a href={profile.profileUrl} className="typo-link" target="_blank">
+              {tidyUrl(profile.profileUrl)}
+            </a>
+          </p>
         )}
       </div>
-      <p>{getUserHashtags(profile).join(" ")}</p>
-      {profile.bio && <p className="max-w-prose">{profile.bio}</p>}
+      {profile.bio && <p className="mt-2 max-w-prose">{profile.bio}</p>}
     </section>
   );
 };
