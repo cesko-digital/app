f5e060784f4956a0ae99fc7154070d536283df8e
Tomáš Znamenáček <zoul@zoul.cz>
2024-07-25 14:05:12 +0200

    Fix type errors

--- app/account/me/route.ts
+++ app/account/me/route.ts
@@ -50,6 +50,7 @@ export async function POST(request: NextRequest): Promise<Response> {
         slackUserRelationId: undefined,
         createdAt: new Date().toISOString(),
         featureFlags: ["registrationV2"],
+        tags: "",
       });
       await logUserCreatedEvent(user);
       return new Response("User profile created.", { status: 201 });
