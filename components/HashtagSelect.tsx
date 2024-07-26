38ef5b2e58485f6182df5262053c1f01a074656b
Tomáš Znamenáček <zoul@zoul.cz>
2024-07-26 11:24:22 +0200

    Add first version of hashtag select component, use in registration form

---
+++ components/HashtagSelect.tsx
@@ -0,0 +1,80 @@
+import Select from "react-select";
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+export type Props = {
+  value: string;
+  className?: string;
+  onChange?: (value: string) => void;
+};
+
+export const HashtagSelect = ({
+  className = "",
+  onChange = () => {},
+  value,
+}: Props) => (
+  <Select
+    options={values.map((v) => ({ label: v, value: v }))}
+    value={decodeSelection(value)}
+    onChange={(selection) => onChange(encodeSelection(selection))}
+    placeholder="Co děláš?"
+    noOptionsMessage={() => "Žádný tag neodpovídá"}
+    className={className}
+    isMulti
+  />
+);
+
+const encodeSelection = (d: readonly Option[]) =>
+  d.map((d) => d.label).join(" ");
+
+const decodeSelection = (s: string): Option[] =>
+  s
+    .split(/ /)
+    .filter((s) => s !== "")
+    .map((name) => ({ label: name, value: name }));
+
+const values = [
+  "#audio",
+  "#backend",
+  "#cloud",
+  "#copywriting",
+  "#css",
+  "#databáze",
+  "#design",
+  "#devops",
+  "#eventy",
+  "#finance",
+  "#frontend",
+  "#fundraising",
+  "#hr",
+  "#html",
+  "#java",
+  "#javascript",
+  "#junior",
+  "#komunity",
+  "#kotlin",
+  "#marketing",
+  "#medior",
+  "#mentor",
+  "#mobile",
+  "#node",
+  "#php",
+  "#pr",
+  "#product-owner",
+  "#projektové-řízení",
+  "#python",
+  "#react",
+  "#scrum",
+  "#senior",
+  "#strategie",
+  "#testování",
+  "#typescript",
+  "#ui",
+  "#ux",
+  "#video",
+  "#výzkum",
+  "#wordpress",
+];
