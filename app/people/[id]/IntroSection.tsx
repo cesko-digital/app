3cff991a34f36991577e9c4f59b5a9929056f317
Tomáš Znamenáček <zoul@zoul.cz>
2024-08-01 10:42:54 +0200

    Display professional profile link as an icon on user profile

--- app/people/[id]/IntroSection.tsx
+++ app/people/[id]/IntroSection.tsx
@@ -1,20 +1,36 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 
 import { useCurrentUser } from "~/components/hooks/user";
+import { iconForUrl } from "~/components/icons";
 import { getUserHashtags, type UserProfile } from "~/src/data/user-profile";
 import { Route } from "~/src/routing";
 
 export const IntroSection = ({ profile }: { profile: UserProfile }) => {
   const isCurrentUser = useCurrentUser(profile.id);
-  const tidyUrl = (s: string) =>
-    s.replace(/^https?:\/\//, "").replace(/\/$/, "");
   return (
     <section>
       <div>
         <div className="flex flex-col justify-between gap-y-2 max-md:mb-3 md:flex-row">
-          <h1 className="typo-title mb-2">{profile.name}</h1>
+          <div className="flex flex-row items-center gap-4">
+            <h1 className="typo-title mb-2">{profile.name}</h1>
+            {profile.profileUrl && (
+              <a
+                href={profile.profileUrl}
+                className="typo-link opacity-60 grayscale hover:opacity-100 hover:grayscale-0"
+                target="_blank"
+              >
+                <Image
+                  src={iconForUrl(profile.profileUrl)}
+                  alt=""
+                  width={24}
+                  height={24}
+                />
+              </a>
+            )}
+          </div>
           {isCurrentUser && (
             <Link href={Route.account} className="btn-inverted">
               Upravit
@@ -24,13 +40,6 @@ export const IntroSection = ({ profile }: { profile: UserProfile }) => {
       </div>
       <div className="text-gravel">
         <p className="mb-1">{getUserHashtags(profile).join(" ")}</p>
-        {profile.profileUrl && (
-          <p>
-            <a href={profile.profileUrl} className="typo-link" target="_blank">
-              {tidyUrl(profile.profileUrl)}
-            </a>
-          </p>
-        )}
       </div>
       {profile.bio && <p className="mt-2 max-w-prose">{profile.bio}</p>}
     </section>
