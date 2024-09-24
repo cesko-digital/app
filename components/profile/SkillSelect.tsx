21db28ba10805d43a736e893b9d1e35a2b8b8b4a
Tomáš Znamenáček <zoul@zoul.cz>
2024-09-24 10:04:46 +0200

    Improve skill select UI

--- components/profile/SkillSelect.tsx
+++ components/profile/SkillSelect.tsx
@@ -1,6 +1,8 @@
-import Select, { createFilter } from "react-select";
+import { useState } from "react";
 
-import defaultTags from "~/src/tags.json";
+import Select from "react-select";
+
+import defaultOptions from "~/src/tags.json";
 
 type Option = {
   name: string;
@@ -19,36 +21,77 @@ export const SkillSelect = ({
   onChange = () => {},
   disabled = false,
   value,
-}: Props) => (
-  <div>
-    <label className="mb-2 block">Co umíš, čím se zabýváš?</label>
-    <Select
-      options={defaultTags.sort((a, b) => a.name.localeCompare(b.name))}
-      getOptionLabel={(option) => option.name}
-      getOptionValue={(option) => option.name}
-      value={decodeSelection(value)}
-      onChange={(selection) => onChange(encodeSelection(selection))}
-      placeholder="copywriting, TypeScript, právo, dotace, operations, …"
-      noOptionsMessage={() => "Žádný tag neodpovídá"}
-      filterOption={createFilter({
-        stringify: (option) => `${option.label}, ${option.data.alsoMatch}`,
-      })}
-      className={className}
-      isDisabled={disabled}
-      isMulti
-    />
-    <p className="typo-caption mt-2 text-balance">
-      Když tohle víme, můžeme ti lépe nabídnout příležitosti k zapojení nebo
-      propojení s ostatními.
-    </p>
-  </div>
-);
+}: Props) => {
+  const sortedOptions: Option[] = defaultOptions.sort(compareOptionsByName);
+  const [options, setOptions] = useState(sortedOptions);
+  return (
+    <div>
+      <label className="mb-2 block">Co umíš, čím se zabýváš?</label>
+      <Select
+        options={options}
+        getOptionLabel={(option) => option.name}
+        getOptionValue={(option) => option.name}
+        value={decodeSelection(value)}
+        onChange={(selection) => onChange(encodeSelection(selection))}
+        placeholder="copywriting, TypeScript, právo, dotace, operations, …"
+        filterOption={() => true /* we have our own filtering logic */}
+        noOptionsMessage={() => "Žádný tag neodpovídá"}
+        onInputChange={(inputValue) =>
+          setOptions(getMatchingOptions(sortedOptions, inputValue))
+        }
+        className={className}
+        isDisabled={disabled}
+        isClearable={false}
+        isMulti
+      />
+      <p className="typo-caption mt-2 text-balance">
+        Když tohle víme, můžeme ti lépe nabídnout příležitosti k zapojení nebo
+        propojení s ostatními.
+      </p>
+    </div>
+  );
+};
 
+const compareOptionsByName = (a: Option, b: Option) =>
+  a.name.localeCompare(b.name);
+
+/** Convert a string into locale-aware lowercase and remove accents */
+const normalize = (s: string) =>
+  s
+    .toLocaleLowerCase()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "");
+
+const getMatchScore = ({ name, alsoMatch }: Option, query: string) => {
+  let score = 0;
+  // Name starts with query, best match
+  if (normalize(name).startsWith(normalize(query))) {
+    score += 5;
+  }
+  // Name includes query, nice
+  if (normalize(name).includes(normalize(query))) {
+    score += 2;
+  }
+  // Search synonyms include query
+  if (alsoMatch && normalize(alsoMatch).includes(normalize(query))) {
+    score += 1;
+  }
+  return score;
+};
+
+const getMatchingOptions = (allOptions: Option[], query: string): Option[] => {
+  return allOptions
+    .filter((o) => getMatchScore(o, query) > 0)
+    .sort((a, b) => getMatchScore(b, query) - getMatchScore(a, query));
+};
+
+/** Encode a list of selected options into a single string */
 const encodeSelection = (d: readonly Option[]) =>
   d.map((d) => d.name).join("; ");
 
+/** Decode a string with a list of options into an array */
 const decodeSelection = (s: string): Option[] =>
   s
     .split(/; /)
     .filter((s) => s !== "")
-    .map((name) => defaultTags.find((tag) => tag.name === name) ?? { name });
+    .map((name) => defaultOptions.find((tag) => tag.name === name) ?? { name });
