864a8ee5d9fc2a953d5cdf7c507e5a0cc759c5e4
Tomáš Znamenáček <zoul@zoul.cz>
2024-10-31 10:47:34 +0100

    Fix useCurrentUser hook

--- components/hooks/user.ts
+++ components/hooks/user.ts
@@ -4,14 +4,17 @@ import { useEffect, useState } from "react";
 
 import { useSession } from "next-auth/react";
 
-import { type OurUser } from "~/src/auth";
+import { assertIsOurUser } from "~/src/utils";
 
 /** Is the user with given ID currently signed in? */
 export const useCurrentUser = (id: string) => {
   const { data: session } = useSession();
   const [isCurrentUser, setCurrentUser] = useState(false);
   useEffect(() => {
-    setCurrentUser((session?.user as OurUser)?.id === id);
+    if (session?.user) {
+      assertIsOurUser(session.user);
+      setCurrentUser(session.user.id === id);
+    }
   }, [session, id]);
   return isCurrentUser;
 };
