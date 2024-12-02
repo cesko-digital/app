32e74f813e305aaf3e76b5ac4bc653bfa660421a
Anezka Muller <anezka.muller@gmail.com>
2024-12-02 17:13:37 +0100

    Update SignedInPage.tsx

--- app/account/SignedInPage.tsx
+++ app/account/SignedInPage.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 
 import { Slack } from "~/components/icons/services";
+import { Diskutuj } from "~/components/icons/services";
 import { SimpleTabBar } from "~/components/TabBar";
 import { Route } from "~/src/routing";
 
@@ -44,6 +45,15 @@ const ToolsTab = () => (
       >
         <Image src={Slack} width={24} height={24} alt="" />
         Přidat se do Slacku
+      </a>
+    </p>
+    <p>
+      <a
+        className="typo-link flex flex-row items-center gap-2"
+        href="https://diskutuj.digital/"
+      >
+        <Image src={Diskutuj} width={24} height={24} alt="" />
+        Přejít na Diskutuj.Digital
       </a>
     </p>
   </div>
