8d5c5b6ca1980c63f2181d16a53e094c92d2437f
Tomáš Znamenáček <zoul@zoul.cz>
2025-06-10 16:34:20 +0200

    Fix & enable Web Finger test for cesko.digital

--- app/website.e2e.ts
+++ app/website.e2e.ts
@@ -1,12 +1,13 @@
 import { expect, test } from "@playwright/test";
 
-test.skip("WebFinger works for cesko.digital", async ({ page }) => {
+test("WebFinger works for cesko.digital", async ({ page }) => {
   const response = await page.request.get(
     "https://cesko.digital/.well-known/host-meta",
   );
   await expect(response).toBeOK();
-  expect(await response.body()).toEqual(`<?xml version="1.0" encoding="UTF-8"?>
+  expect(await response.text()).toEqual(`<?xml version="1.0" encoding="UTF-8"?>
 <XRD xmlns="http://docs.oasis-open.org/ns/xri/xrd-1.0">
   <Link rel="lrdd" template="https://mastodon.cesko.digital/.well-known/webfinger?resource={uri}"/>
-</XRD>`);
+</XRD>
+`);
 });
